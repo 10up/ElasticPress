dde25e8d17ab28caa46583129178d457ef0bb6db
Taylor Lovett <admin@taylorlovett.com>
2014-06-03 23:34:43 -0400

    Resetup tests with patchwork; simple test for testing single site sync create/delete

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,15 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
-
-echo $_tests_dir . "\n\n";
-exit;
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
-
-require_once 'vendor/autoload.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../elasticsearch.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
+require 'vendor/antecedent/patchwork/Patchwork.php';
 
+require $_tests_dir . '/includes/bootstrap.php';
