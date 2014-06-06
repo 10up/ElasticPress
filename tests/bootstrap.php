e8a75fe674ea5fb4a89c4e921cdcb887efa6a14b
Taylor Lovett <admin@taylorlovett.com>
2014-06-06 01:23:11 -0400

    Start multisite syncing tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
--- tests/test-core.php
+++ tests/test-core.php
@@ -10,6 +10,8 @@ class ESTestCore extends WP_UnitTestCase {
 	protected $fired_actions = array();
 
 	public function setUp() {
+		parent::setUp();
+
 		$user = @wp_signon(
 			array(
 				'user_login' => 'admin',
@@ -36,6 +38,8 @@ class ESTestCore extends WP_UnitTestCase {
 	}
 
 	public function tearDown() {
+		parent::tearDown();
+
 		$this->wp_remote_request_mock['args'] = false;
 		$this->wp_remote_request_mock['return'] = false;
 		$this->fired_actions = array();
@@ -53,20 +57,77 @@ class ESTestCore extends WP_UnitTestCase {
 		return $config;
 	}
 
-	protected function _createAndSyncPost( $config ) {
+	protected function _configureMultiSite() {
+		$config = array();
+
+		$config[0] = array(
+			'host' => 'http://127.0.0.1:9200',
+			'index_name' => 'test-index',
+			'cross_site_search_active' => 1,
+		);
+
+		es_update_option( $config[0], 0 );
+
+		$config[1] = array(
+			'host' => 'http://888.0.0.1:9100',
+			'post_types' => array( 'post' ),
+			'index_name' => 'test-index-1',
+		);
+
+		es_update_option( $config[1], 1 );
+
+		$blog_ids = $this->factory->blog->create_many( 2 );
+
+		$i = 2;
+
+		foreach ( $blog_ids as $blog_id ) {
+			$config[$blog_id] = array(
+				'host' => 'http://999.0.0.1:9100',
+				'post_types' => ( $i % 2 == 0 ) ? array( 'post', 'page' ) : array( 'page' ),
+				'index_name' => 'test-index-' . $i,
+			);
+
+			es_update_option( $config[$blog_id], $blog_id );
+
+			$i++;
+		}
+
+		return $config;
+	}
+
+	protected function _createAndSyncPost( $site_id = null, $cross_site = false ) {
+		if ( $site_id != null ) {
+			switch_to_blog( $site_id );
+		}
+
+		$config = es_get_option( $site_id );
+		$index_name = $config['index_name'];
+		$host = $config['host'];
+
+		if ( $cross_site ) {
+			$global_config = es_get_option( 0 );
+			$index_name = $global_config['index_name'];
+			$host = $global_config['host'];
+		}
+
 		$post_id = wp_insert_post( array(
-			'post_type' => 'post',
+			'post_type' => $config['post_types'][0],
 			'post_status' => 'draft',
 			'author' => 1,
-			'post_title' => 'Test Post'
+			'post_title' => 'Test Post ' . time(),
 		) );
+
+		$es_id = $post_id;
+		if ( $site_id > 1 ) {
+			$es_id = $site_id . 'ms' . $post_id;
+		}
 
 		$response = array(
 			'headers' => array(
 				'content-type' => 'application/json; charset=UTF-8',
 				'content-length' => '*',
 			),
-			'body' => '{"_index":"test-index","_type":"post","_id":"' . $post_id . '","_version":1,"created":true}',
+			'body' => '{"_index":"' . $index_name . '","_type":"post","_id":"' . $es_id . '","_version":1,"created":true}',
 			'response' => array(
 				'code' => 200,
 				'message' => 'OK',
@@ -75,10 +136,15 @@ class ESTestCore extends WP_UnitTestCase {
 			'filename' => null,
 		);
 
-		$this->wp_remote_request_mock['args'] = array( $config['host'] . '/' . $config['index_name'] . '/post/' . $post_id );
+		$this->wp_remote_request_mock['args'] = array( $host . '/' . $index_name . '/post/' . $es_id );
+
 		$this->wp_remote_request_mock['return'] = $response;
 
 		wp_publish_post( $post_id );
+
+		if ( $site_id != null ) {
+			restore_current_blog();
+		}
 
 		return $post_id;
 	}
@@ -97,10 +163,25 @@ class ESTestCore extends WP_UnitTestCase {
 		}
 	}
 
+	public function testMultiSiteConfigSet() {
+		$config = $this->_configureMultiSite();
+
+		$option = get_site_option( 'es_config_by_site', array() );
+
+		$this->assertTrue( count( $option ) > 1 );
+
+		foreach ( $option as $site_id => $site_config ) {
+
+			foreach ( $site_config as $key => $value ) {
+				$this->assertEquals( $site_config[$key], $config[$site_id][$key] );
+			}
+		}
+	}
+
 	public function testSingleSiteSearchBasic() {
 		$config = $this->_configureSingleSite();
 
-		$post_id = $this->_createAndSyncPost( $config );
+		$post_id = $this->_createAndSyncPost();
 		$post = get_post( $post_id );
 
 		$response = array(
@@ -134,12 +215,12 @@ class ESTestCore extends WP_UnitTestCase {
 		}
 	}
 
-	public function testPostCreateDeleteSync() {
+	public function testSingleSitePostCreateDeleteSync() {
 		$config = $this->_configureSingleSite();
 
 		// First let's create a post to play with
 
-		$post_id = $this->_createAndSyncPost( $config );
+		$post_id = $this->_createAndSyncPost();
 
 		// Let's test to see if this post was sent to the index
 
@@ -179,5 +260,76 @@ class ESTestCore extends WP_UnitTestCase {
 		$post_indexed = es_post_indexed( $post_id );
 
 		$this->assertFalse( $post_indexed );
+	}
+
+	public function testMultiSitePostCreateDeleteSync() {
+		$config = $this->_configureMultiSite();
+
+		// First let's create some posts across the network
+		$post_ids_by_site = array();
+
+		$sites = wp_get_sites();
+		foreach ( $sites as $site ) {
+			$post_ids_by_site[$site['blog_id']] = array();
+
+			$post_ids_by_site[$site['blog_id']][] = $this->_createAndSyncPost( $site['blog_id'], true );
+			$post_ids_by_site[$site['blog_id']][] = $this->_createAndSyncPost( $site['blog_id'], true );
+			$post_ids_by_site[$site['blog_id']][] = $this->_createAndSyncPost( $site['blog_id'], true );
+		}
+
+
+		// Let's test to see if this post was sent to the index
+
+		foreach( $post_ids_by_site as $blog_id => $post_ids ) {
+
+			switch_to_blog( $blog_id );
+
+			foreach( $post_ids as $post_id ) {
+				$es_id = get_post_meta( $post_id, 'es_id', true );
+				$correct_es_id = ( $blog_id <= 1 ) ? $post_id : $blog_id . 'ms' . $post_id;
+
+				$this->assertEquals( $es_id, $correct_es_id );
+
+				add_action( 'es_delete_post', function( $args ) {
+					$this->fired_actions['es_delete_post'] = true;
+				} );
+
+				wp_delete_post( $post_id );
+
+				// Check if ES delete action has been properly fired
+
+				$this->assertTrue( ! empty( $this->fired_actions['es_delete_post'] ) );
+
+				// Now let's make sure the post is not indexed
+
+				$response = array(
+					'headers' => array(
+						'content-type' => 'application/json; charset=UTF-8',
+						'content-length' => '*',
+					),
+					'body' => '{"_index":"test-index","_type":"post","_id":"' . $es_id . '","found":false}',
+					'response' => array(
+						'code' => 404,
+						'message' => 'Not Found',
+					),
+					'cookies' => array(),
+					'filename' => null,
+				);
+
+				$this->wp_remote_request_mock['args'] = array( $config[0]['host'] . '/' . $config[0]['index_name'] . '/post/' . $es_id );
+				$this->wp_remote_request_mock['return'] = $response;
+
+				$post_indexed = es_post_indexed( $post_id, $blog_id, 0 );
+
+				$this->assertFalse( $post_indexed );
+
+				$this->wp_remote_request_mock['args'] = false;
+				$this->wp_remote_request_mock['return'] = false;
+				$this->fired_actions = array();
+				remove_all_actions( 'es_delete_post' );
+			}
+
+			restore_current_blog();
+		}
 	}
 }
