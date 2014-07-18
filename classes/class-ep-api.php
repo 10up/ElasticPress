fbe44e6433d9b52d1953fa3689fa7841707503b1
Aaron Holbrook <aaron@10up.com>
2014-07-17 20:19:16 -0500

    Add put mapping function to ep-api

--- classes/class-ep-api.php
+++ classes/class-ep-api.php
@@ -221,6 +221,230 @@ class EP_API {
 		// Return our status and cache it
 		return $this->is_alive[ $site_id ] = $is_alive;
 	}
+
+	public function put_mapping( $site_id = null ) {
+		$mapping = array(
+			'settings' => array(
+				'analysis' => array(
+					'analyzer' => array(
+						'default' => array(
+							'tokenizer' => 'standard',
+							'filter' => array( 'standard', 'ewp_word_delimiter', 'lowercase', 'stop', 'ewp_snowball' ),
+							'language' => 'English'
+						),
+						'shingle_analyzer' => array(
+							'type' => 'custom',
+							'tokenizer' => 'standard',
+							'filter' => array( 'lowercase', 'shingle_filter' )
+						),
+					),
+					'filter' => array(
+						'shingle_filter' => array(
+							'type' => 'shingle',
+							'min_shingle_size' => 2,
+							'max_shingle_size' => 5
+						),
+						'ewp_word_delimiter' => array(
+							'type' => 'word_delimiter',
+							'preserve_original' => true
+						),
+						'ewp_snowball' => array(
+							'type' => 'snowball',
+							'language' => 'English'
+						),
+						'edge_ngram' => array(
+							'side' => 'front',
+							'max_gram' => 10,
+							'min_gram' => 3,
+							'type' => 'edgeNGram'
+						)
+					)
+				)
+			),
+			'mappings' => array(
+				'post' => array(
+					"date_detection" => false,
+					"dynamic_templates" => array(
+						array(
+							"template_meta" => array(
+								"path_match" => "post_meta.*",
+								"mapping" => array(
+									"type" => "multi_field",
+									"path" => "full",
+									"fields" => array(
+										"{name}" => array(
+											"type" => "string",
+											"index" => "analyzed"
+										),
+										"raw" => array(
+											"type" => "string",
+											"index" => "not_analyzed",
+											'include_in_all' => false
+										)
+									)
+								)
+							)
+						),
+						array(
+							"template_terms" => array(
+								"path_match" => "terms.*",
+								"mapping" => array(
+									"type" => "object",
+									"path" => "full",
+									"properties" => array(
+										"name" => array(
+											"type" => "string"
+										),
+										"term_id" => array(
+											"type" => "long"
+										),
+										"parent" => array(
+											"type" => "long"
+										),
+										"slug" => array(
+											"type" => "string",
+											"index" => "not_analyzed"
+										)
+									)
+								)
+							)
+						),
+						array(
+							"term_suggest" => array(
+								"path_match" => "term_suggest_*",
+								"mapping" => array(
+									"type" => "completion",
+									"analyzer" => "default",
+								)
+							)
+						)
+					),
+					"_all" => array(
+						"analyzer" => "simple"
+					),
+					'properties' => array(
+						'post_id' => array(
+							'type' => 'long',
+							'index' => 'not_analyzed',
+							'include_in_all' => false
+						),
+						'post_author' => array(
+							'type' => 'object',
+							'path' => 'full',
+							'properties' => array(
+								'display_name' => array(
+									'type' => 'string'
+								),
+								'login' => array(
+									'type' => 'string',
+									'index' => 'not_analyzed'
+								)
+							)
+						),
+						'post_date' => array(
+							'type' => 'date',
+							'format' => 'YYYY-MM-dd HH:mm:ss',
+							'include_in_all' => false
+						),
+						'post_date_gmt' => array(
+							'type' => 'date',
+							'format' => 'YYYY-MM-dd HH:mm:ss',
+							'include_in_all' => false
+						),
+						'post_title' => array(
+							'type' => 'string',
+							'_boost'  => 3.0,
+							'store'  => 'yes',
+							'analyzer' => 'standard'
+						),
+						'post_excerpt' => array(
+							'type' => 'string',
+							'_boost'  => 2.0
+						),
+						'post_content' => array(
+							'type' => 'string',
+							'analyzer' => 'default'
+						),
+						'post_status' => array(
+							'type' => 'string',
+							'index' => 'no'
+						),
+						'post_name' => array(
+							'type' => 'multi_field',
+							'fields' => array(
+								'post_name' => array(
+									'type' => 'string'
+								),
+								'raw' => array(
+									'type' => 'string',
+									'index' => 'not_analyzed',
+									'include_in_all' => false
+								)
+							)
+						),
+						'post_modified' => array(
+							'type' => 'date',
+							'format' => 'YYYY-MM-dd HH:mm:ss',
+							'include_in_all' => false
+						),
+						'post_modified_gmt' => array(
+							'type' => 'date',
+							'format' => 'YYYY-MM-dd HH:mm:ss',
+							'include_in_all' => false
+						),
+						'post_parent' => array(
+							'type' => 'long',
+							'index' => 'not_analyzed',
+							'include_in_all' => false
+						),
+						'post_type' => array(
+							'type' => 'multi_field',
+							'fields' => array(
+								'post_type' => array(
+									'type' => 'string'
+								),
+								'raw' => array(
+									'type' => 'string',
+									'index' => 'not_analyzed',
+									'include_in_all' => false
+								)
+							)
+						),
+						'post_mime_type' => array(
+							'type' => 'string',
+							'index' => 'not_analyzed',
+							'include_in_all' => false
+						),
+						'permalink' => array(
+							'type' => 'string'
+						),
+						'terms' => array(
+							"type" => "object"
+						),
+						'post_meta' => array(
+							'type' => 'object'
+						)
+					)
+				)
+			)
+		);
+
+		$mapping = apply_filters( 'ep_config_mapping', $mapping );
+
+		$index_url = ep_get_index_url( $site_id );
+
+		$url = $index_url . '/post/_mapping/';
+
+		$request = wp_remote_request( $url, array( 'body' => json_encode( $mapping ), 'method' => 'PUT' ) );
+
+		if ( ! is_wp_error( $request ) ) {
+			$response_body = wp_remote_retrieve_body( $request );
+
+			return json_decode( $response_body );
+		}
+
+		return false;
+	}
 }
 
 EP_API::factory();
@@ -247,4 +471,8 @@ function ep_delete_post( $post_id, $site_id = null, $host_site_id = null ) {
 
 function ep_is_alive( $site_id = null ) {
 	return EP_API::factory()->is_alive( $site_id );
+}
+
+function ep_put_mapping( $site_id = null ) {
+	return EP_API::factory()->put_mapping( $site_id );
 }
