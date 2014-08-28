1795755c9d535e06b87697317f053cf1ed353298
Taylor Lovett <tlovett88@gmail.com>
2014-08-28 13:37:15 -0400

    Tag sync test

--- tests/test-single-site.php
+++ tests/test-single-site.php
@@ -56,7 +56,7 @@ class EPTestSingleSite extends WP_UnitTestCase {
 	 *
 	 * @since 0.9
 	 */
-	public function testSingleSitePostSync() {
+	public function testPostSync() {
 		add_action( 'ep_sync_on_transition', function() {
 			$this->fired_actions['ep_sync_on_transition'] = true;
 		}, 10, 0 );
@@ -74,7 +74,7 @@ class EPTestSingleSite extends WP_UnitTestCase {
 	 *
 	 * @since 0.9
 	 */
-	public function testSingleSiteWPQuerySearchContent() {
+	public function testWPQuerySearchContent() {
 		$post_ids = array();
 
 		$post_ids[0] = ep_create_and_sync_post();
@@ -116,6 +116,8 @@ class EPTestSingleSite extends WP_UnitTestCase {
 			$this->assertEquals( $post->post_excerpt, $wp_post->post_excerpt );
 			$this->assertEquals( $post->site_id, get_current_blog_id() );
 		}
+
+		wp_reset_postdata();
 	}
 
 	/**
@@ -123,7 +125,7 @@ class EPTestSingleSite extends WP_UnitTestCase {
 	 *
 	 * @since 0.9
 	 */
-	public function testSingleSiteWPQuerySearchTitle() {
+	public function testWPQuerySearchTitle() {
 		$post_ids = array();
 
 		$post_ids[0] = ep_create_and_sync_post();
@@ -146,25 +148,60 @@ class EPTestSingleSite extends WP_UnitTestCase {
 
 		$this->assertEquals( $query->post_count, 3 );
 		$this->assertEquals( $query->found_posts, 3 );
-
-		while ( $query->have_posts() ) {
-			$query->the_post();
-
-			global $post;
-
-			$wp_post = get_post( get_the_ID() );
-
-			$this->assertEquals( $post->post_title, get_the_title() );
-			$this->assertEquals( $post->post_content, get_the_content() );
-			$this->assertEquals( $post->post_date, $wp_post->post_date );
-			$this->assertEquals( $post->post_modified, $wp_post->post_modified );
-			$this->assertEquals( $post->post_date_gmt, $wp_post->post_date_gmt );
-			$this->assertEquals( $post->post_modified_gmt, $wp_post->post_modified_gmt );
-			$this->assertEquals( $post->post_name, $wp_post->post_name );
-			$this->assertEquals( $post->post_parent, $wp_post->post_parent );
-			$this->assertEquals( $post->post_excerpt, $wp_post->post_excerpt );
-			$this->assertEquals( $post->site_id, get_current_blog_id() );
-		}
 	}
 
+	/**
+	 * Make sure proper taxonomies are synced with post. Hidden taxonomies should be skipped!
+	 *
+	 * @since 0.1.1
+	 */
+	public function testPostTermSync() {
+
+		add_filter( 'ep_post_sync_args', function( $post_args ) {
+			$this->applied_filters['ep_post_sync_args'] = $post_args;
+
+			return $post_args;
+		}, 10, 1 );
+
+		$post_id = ep_create_and_sync_post( array(
+			'tags_input' => array( 'test-tag', 'test-tag2' )
+		) );
+
+		// Check if ES post sync filter has been triggered
+		$this->assertTrue( ! empty( $this->applied_filters['ep_post_sync_args'] ) );
+
+		// Check if tag was synced
+		$post = ep_get_post( $post_id );
+		$this->assertTrue( ! empty( $post['terms']['post_tag'] ) );
+	}
+
+	/**
+	 * Test WP Query search on post excerpt
+	 *
+	 * @since 0.9
+	 */
+	public function testWPQuerySearchExcerpt() {
+		$post_ids = array();
+
+		$post_ids[0] = ep_create_and_sync_post();
+		$post_ids[1] = ep_create_and_sync_post();
+		$post_ids[2] = ep_create_and_sync_post( array( 'post_excerpt' => 'findme test' ) );
+		$post_ids[3] = ep_create_and_sync_post();
+		$post_ids[4] = ep_create_and_sync_post();
+
+		$args = array(
+			's' => 'findme',
+		);
+
+		add_action( 'ep_wp_query_search', function() {
+			$this->fired_actions['ep_wp_query_search'] = true;
+		}, 10, 0 );
+
+		$query = new WP_Query( $args );
+
+		$this->assertTrue( ! empty( $this->fired_actions['ep_wp_query_search'] ) );
+
+		$this->assertEquals( $query->post_count, 1 );
+		$this->assertEquals( $query->found_posts, 1 );
+	}
 }
