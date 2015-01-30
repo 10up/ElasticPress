1b23f305a3c3eac9f0eadb6a464f52fa9d6d6844
Sebastian Becker <bastibeckr@gmail.com>
2015-01-30 21:12:33 +0100

    Fix whitespace + required "comma after last element of array"

--- classes/class-ep-api.php
+++ classes/class-ep-api.php
@@ -304,11 +304,11 @@ class EP_API {
 						'shingle_filter' => array(
 							'type' => 'shingle',
 							'min_shingle_size' => 2,
-							'max_shingle_size' => 5
+							'max_shingle_size' => 5,
 						),
 						'ewp_word_delimiter' => array(
 							'type' => 'word_delimiter',
-							'preserve_original' => true
+							'preserve_original' => true,
 						),
 						'ewp_snowball' => array(
 							'type' => 'snowball',
@@ -318,77 +318,77 @@ class EP_API {
 							'side' => 'front',
 							'max_gram' => 10,
 							'min_gram' => 3,
-							'type' => 'edgeNGram'
+							'type' => 'edgeNGram',
 						)
 					)
 				)
 			),
 			'mappings' => array(
 				'post' => array(
-					"date_detection" => false,
-					"dynamic_templates" => array(
+					'date_detection' => false,
+					'dynamic_templates' => array(
 						array(
-							"template_meta" => array(
-								"path_match" => "post_meta.*",
-								"mapping" => array(
-									"type" => "multi_field",
-									"path" => "full",
-									"fields" => array(
-										"{name}" => array(
-											"type" => "string",
-											"index" => "analyzed"
+							'template_meta' => array(
+								'path_match' => 'post_meta.*',
+								'mapping' => array(
+									'type' => 'multi_field',
+									'path' => 'full',
+									'fields' => array(
+										'{name}' => array(
+											'type' => 'string',
+											'index' => 'analyzed',
 										),
-										"raw" => array(
-											"type" => "string",
-											"index" => "not_analyzed",
-											'include_in_all' => false
+										'raw' => array(
+											'type' => 'string',
+											'index' => 'not_analyzed',
+											'include_in_all' => false,
 										)
 									)
 								)
 							)
 						),
 						array(
-							"template_terms" => array(
-								"path_match" => "terms.*",
-								"mapping" => array(
-									"type" => "object",
-									"path" => "full",
-									"properties" => array(
-										"name" => array(
-											"type" => "string"
+							'template_terms' => array(
+								'path_match' => 'terms.*',
+								'mapping' => array(
+									'type' => 'object',
+									'path' => 'full',
+									'properties' => array(
+										'name' => array(
+											'type' => 'string',
 										),
-										"term_id" => array(
-											"type" => "long"
+										'term_id' => array(
+											'type' => 'long',
 										),
-										"parent" => array(
-											"type" => "long"
+										'parent' => array(
+											'type' => 'long',
 										),
-										"slug" => array(
-											"type" => "string",
-											"index" => "not_analyzed"
+										'slug' => array(
+											'type' => 'string',
+											'index' => 'not_analyzed',
 										)
 									)
 								)
 							)
 						),
 						array(
-							"term_suggest" => array(
-								"path_match" => "term_suggest_*",
-								"mapping" => array(
-									"type" => "completion",
-									"analyzer" => "default",
+							'term_suggest' => array(
+								'path_match' => 'term_suggest_*',
+								'mapping' => array(
+									'type' => 'completion',
+									'analyzer' => 'default',
 								)
 							)
-						)
+						),
 					),
-					"_all" => array(
-						"analyzer" => "simple"
+					'_all' => array(
+						'analyzer' => 'simple',
 					),
 					'properties' => array(
 						'post_id' => array(
 							'type' => 'long',
 							'index' => 'not_analyzed',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_author' => array(
 							'type' => 'object',
@@ -404,24 +404,24 @@ class EP_API {
 								),
 								'id' => array(
 									'type' => 'long',
-									'index' => 'not_analyzed'
+									'index' => 'not_analyzed',
 								),
 								'raw' => array(
 									'type' => 'string',
 									'index' => 'not_analyzed',
-									'include_in_all' => false
+									'include_in_all' => false,
 								)
 							)
 						),
 						'post_date' => array(
 							'type' => 'date',
 							'format' => 'YYYY-MM-dd HH:mm:ss',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_date_gmt' => array(
 							'type' => 'date',
 							'format' => 'YYYY-MM-dd HH:mm:ss',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_title' => array(
 							'type' => 'multi_field',
@@ -429,80 +429,80 @@ class EP_API {
 								'post_title' => array(
 									'type' => 'string',
 									'analyzer' => 'standard',
-									'store' => 'yes'
+									'store' => 'yes',
 								),
 								'raw' => array(
 									'type' => 'string',
 									'index' => 'not_analyzed',
-									'include_in_all' => false
+									'include_in_all' => false,
 								)
 							)
 						),
 						'post_excerpt' => array(
-							'type' => 'string'
+							'type' => 'string',
 						),
 						'post_content' => array(
 							'type' => 'string',
-							'analyzer' => 'default'
+							'analyzer' => 'default',
 						),
 						'post_status' => array(
 							'type' => 'string',
-							'index' => 'no'
+							'index' => 'no',
 						),
 						'post_name' => array(
 							'type' => 'multi_field',
 							'fields' => array(
 								'post_name' => array(
-									'type' => 'string'
+									'type' => 'string',
 								),
 								'raw' => array(
 									'type' => 'string',
 									'index' => 'not_analyzed',
-									'include_in_all' => false
+									'include_in_all' => false,
 								)
 							)
 						),
 						'post_modified' => array(
 							'type' => 'date',
 							'format' => 'YYYY-MM-dd HH:mm:ss',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_modified_gmt' => array(
 							'type' => 'date',
 							'format' => 'YYYY-MM-dd HH:mm:ss',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_parent' => array(
 							'type' => 'long',
 							'index' => 'not_analyzed',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'post_type' => array(
 							'type' => 'multi_field',
 							'fields' => array(
 								'post_type' => array(
-									'type' => 'string'
+									'type' => 'string',
 								),
 								'raw' => array(
 									'type' => 'string',
 									'index' => 'not_analyzed',
-									'include_in_all' => false
+									'include_in_all' => false,
 								)
 							)
 						),
 						'post_mime_type' => array(
 							'type' => 'string',
 							'index' => 'not_analyzed',
-							'include_in_all' => false
+							'include_in_all' => false,
 						),
 						'permalink' => array(
-							'type' => 'string'
+							'type' => 'string',
 						),
 						'terms' => array(
-							"type" => "object"
+							'type' => 'object',
 						),
 						'post_meta' => array(
-							'type' => 'object'
+							'type' => 'object',
 						)
 					)
 				)
@@ -641,7 +641,7 @@ class EP_API {
 					'term_id' => $term->term_id,
 					'slug'    => $term->slug,
 					'name'    => $term->name,
-					'parent'  => $term->parent
+					'parent'  => $term->parent,
 				);
 			}
 		}
@@ -770,7 +770,7 @@ class EP_API {
 		if ( ! empty( $args['tax_query'] ) ) {
 			$tax_filter = array();
 
-			foreach( $args['tax_query'] as $single_tax_query ) {
+			foreach ( $args['tax_query'] as $single_tax_query ) {
 				if ( ! empty( $single_tax_query['terms'] ) && ! empty( $single_tax_query['field'] ) && 'slug' === $single_tax_query['field'] ) {
 					$terms = (array) $single_tax_query['terms'];
 
@@ -881,7 +881,7 @@ class EP_API {
 							'like_text' => '',
 							'min_similarity' => apply_filters( 'ep_min_similarity', 0.75 )
 						),
-					)
+					),
 				),
 			),
 		);
