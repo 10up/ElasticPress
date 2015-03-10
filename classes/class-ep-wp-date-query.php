80439ce2ef05ced9b94626141b2755243356b74f
Joey Blake <joeyblake@gmail.com>
2015-03-10 15:14:52 -0400

    support for the rest of the compare operators

--- classes/class-ep-wp-date-query.php
+++ classes/class-ep-wp-date-query.php
@@ -4,6 +4,7 @@ class EP_WP_Date_Query extends WP_Date_Query {
 
 	public function get_es_filter() {
 		$filter = $this->get_es_filter_for_clauses();
+
 		return $filter;
 	}
 
@@ -18,20 +19,33 @@ class EP_WP_Date_Query extends WP_Date_Query {
 	}
 
 	public function get_compare( $query ) {
-		if ( ! empty( $query['compare'] ) && in_array( $query['compare'], array( '=', '!=', '>', '>=', '<', '<=', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN' ) ) )
+		if ( ! empty( $query['compare'] ) && in_array( $query['compare'], array(
+				'=',
+				'!=',
+				'>',
+				'>=',
+				'<',
+				'<=',
+				'IN',
+				'NOT IN',
+				'BETWEEN',
+				'NOT BETWEEN'
+			) )
+		) {
 			return strtoupper( $query['compare'] );
+		}
 
 		return $this->compare;
 	}
 
 	protected function get_es_filter_for_query( $query, $depth = 0 ) {
 		$filter_chunks = array(
-			'filters'  => array(),
+			'filters' => array(),
 		);
 
 		$filter_array = array();
 		echo '<pre>';
-		var_dump($query);
+		var_dump( $query );
 		echo '</pre>';
 
 		foreach ( $query as $key => $clause ) {
@@ -52,7 +66,7 @@ class EP_WP_Date_Query extends WP_Date_Query {
 
 					// This is a subquery, so we recurse.
 				} else {
-					$clause_filter = $this->get_es_filter_for_query( $clause, $depth + 1 );
+					$clause_filter              = $this->get_es_filter_for_query( $clause, $depth + 1 );
 					$filter_chunks['filters'][] = $clause_filter;
 				}
 			}
@@ -60,7 +74,7 @@ class EP_WP_Date_Query extends WP_Date_Query {
 
 		echo '<pre>';
 		echo 'filter chunks';
-		var_dump($filter_chunks['filters']);
+		var_dump( $filter_chunks['filters'] );
 		echo '</pre>';
 
 		if ( empty( $relation ) ) {
@@ -70,20 +84,25 @@ class EP_WP_Date_Query extends WP_Date_Query {
 		if ( 'AND' === $relation ) {
 			$filter_array['and'] = array();
 			foreach ( $filter_chunks['filters'] as $key => $filter ) {
-				$filter_type = key($filter);
-				var_dump($filter);
+				$filter_type = key( $filter );
+				var_dump( $filter );
 				if ( 'date_terms' === $filter_type ) {
 					$filter_array['and'] = array(
-						'bool' => array(
-							'must' => $filter['date_terms'],
-						)
+						'bool' => $filter['date_terms'],
 					);
 				} else if ( 'range' === $filter_type ) {
-					if( !is_array( $filter_array['and']['range'] ) )
+					if ( ! is_array( $filter_array['and']['range'] ) && ! array_key_exists('not', $filter['range'] ) ) {
 						$filter_array['and']['range'] = array();
-
-					$range_column = key($filter['range'][0]);
-					$filter_array['and']['range'][$range_column] = $filter['range'][0][$range_column];
+					} else if ( array_key_exists('not', $filter['range'] ) ) {
+						$filter_array['and']['not']['range'] = array();
+					}
+					if ( array_key_exists('not', $filter['range'] ) ) {
+						$range_column                                  = key( $filter['range']['not'] );
+						$filter_array['and']['not']['range'][ $range_column ] = $filter['range']['not'][ $range_column ];
+					} else {
+						$range_column                                  = key( $filter['range'][0] );
+						$filter_array['and']['range'][ $range_column ] = $filter['range'][0][ $range_column ];
+					}
 
 				}
 			}
@@ -114,59 +133,97 @@ class EP_WP_Date_Query extends WP_Date_Query {
 		}
 
 		// Range queries.
-		if ( ! empty( $query['after'] ) || ! empty( $query['before'] ) )
-			$range_filter[$column] = array();
+		if ( ! empty( $query['after'] ) || ! empty( $query['before'] ) ) {
+			$range_filter[ $column ] = array();
+		}
 
-		if ( ! empty( $query['after'] ) )
-			$range_filter[$column][$gt] = $this->build_mysql_datetime( $query['after'] );
+		if ( ! empty( $query['after'] ) ) {
+			$range_filter[ $column ][ $gt ] = $this->build_mysql_datetime( $query['after'] );
+		}
 
-		if ( ! empty( $query['before'] ) )
-			$range_filter[$column][$lt] = $this->build_mysql_datetime( $query['before'] );
+		if ( ! empty( $query['before'] ) ) {
+			$range_filter[ $column ][ $lt ] = $this->build_mysql_datetime( $query['before'] );
+		}
 
-		if ( ! empty( $query['after'] ) || ! empty( $query['before'] ) )
-			$filter_parts['range'] = array($range_filter);
+		if ( ! empty( $query['after'] ) || ! empty( $query['before'] ) ) {
+			$filter_parts['range'] = array( $range_filter );
+		}
 
 		// Specific value queries.
 
 		$date_parameters = array(
-			'year' => ! empty( $query['year'] ) ? $query['year'] :false,
-			'month' => ! empty( $query['month']) ? $query['month'] :false,
-			'monthnum' => ! empty( $query['monthnum']) ? $query['monthnum'] :false,
-			'w' => ! empty( $query['w']) ?  $query['w'] :false,
-			'week' => ! empty( $query['week']) ?  $query['week'] :false,
-			'dayofyear' => ! empty( $query['dayofyear']) ?  $query['dayofyear'] :false,
-			'day' => ! empty( $query['day']) ?  $query['day'] :false,
-			'dayofweek' => ! empty( $query['dayofweek']) ?  $query['dayofweek'] :false,
-			'dayofweek_iso' => ! empty( $query['dayofweek_iso']) ?  $query['dayofweek_iso'] :false,
-			'hour' => ! empty( $query['hour']) ? $query['hour'] :false,
-			'minute' => ! empty( $query['minute']) ? $query['minute'] :false,
-			'second' => ! empty( $query['second'] ) ? $query['second'] :false,
-			'm' => ! empty( $query['m'] ) ? $query['m'] :false, // yearmonth
+			'year'          => ! empty( $query['year'] ) ? $query['year'] : false,
+			'month'         => ! empty( $query['month'] ) ? $query['month'] : false,
+			'monthnum'      => ! empty( $query['monthnum'] ) ? $query['monthnum'] : false,
+			'w'             => ! empty( $query['w'] ) ? $query['w'] : false,
+			'week'          => ! empty( $query['week'] ) ? $query['week'] : false,
+			'dayofyear'     => ! empty( $query['dayofyear'] ) ? $query['dayofyear'] : false,
+			'day'           => ! empty( $query['day'] ) ? $query['day'] : false,
+			'dayofweek'     => ! empty( $query['dayofweek'] ) ? $query['dayofweek'] : false,
+			'dayofweek_iso' => ! empty( $query['dayofweek_iso'] ) ? $query['dayofweek_iso'] : false,
+			'hour'          => ! empty( $query['hour'] ) ? $query['hour'] : false,
+			'minute'        => ! empty( $query['minute'] ) ? $query['minute'] : false,
+			'second'        => ! empty( $query['second'] ) ? $query['second'] : false,
+			'm'             => ! empty( $query['m'] ) ? $query['m'] : false, // yearmonth
 		);
 
-		foreach( $date_parameters as $param => $value ) {
+		foreach ( $date_parameters as $param => $value ) {
 			if ( false === $value ) {
-				unset( $date_parameters[$param] );
+				unset( $date_parameters[ $param ] );
 			}
 		}
 
-		if (  $date_parameters ) {
+		if ( $date_parameters ) {
 			EP_WP_Date_Query::validate_date_values( $date_parameters );
 			$date_terms = array();
+			$bool_check = 'must';
 			foreach ( $date_parameters as $param => $value ) {
 				if ( '=' === $compare ) {
 					$date_terms[]['term']["date_terms.{$param}"] = $value;
+				} else if ( 'IN' === $compare ) {
+					$bool_check = 'should';
+					foreach ( $value as $in_value ) {
+						$date_terms[]['term']["date_terms.{$param}"] = $in_value;
+					}
+				} else if ( 'NOT IN' === $compare ) {
+					$bool_check = 'must_not';
+					foreach ( $value as $in_value ) {
+						$date_terms[]['term']["date_terms.{$param}"] = $in_value;
+					}
+				} else if ( 'BETWEEN' === $compare ) {
+					$range_filter[ "date_terms.{$param}" ] = array();
+					$range_filter[ "date_terms.{$param}" ][ 'gt' ] = $value[0];
+					$range_filter[ "date_terms.{$param}" ][ 'lt' ] = $value[1];
+					$filter_parts['range'] = array( $range_filter );
+				} else if ( 'NOT BETWEEN' === $compare ) {
+					$range_filter[ "date_terms.{$param}" ] = array();
+					$range_filter[ "date_terms.{$param}" ][ 'gt' ] = $value[0];
+					$range_filter[ "date_terms.{$param}" ][ 'lt' ] = $value[1];
+					$filter_parts['range'] = array( 'not' => $range_filter );
+				} else if ( strpos($compare, '>') !== FALSE ) {
+					$range = ( strpos($compare, '=') !== FALSE ) ? 'gte' : 'gt';
+					$range_filter[ "date_terms.{$param}" ] = array();
+					$range_filter[ "date_terms.{$param}" ][ $range ] = $value;
+					$filter_parts['range'] = array( $range_filter );
+				} else if ( strpos($compare, '<') !== FALSE ) {
+					$range = ( strpos($compare, '=') !== FALSE ) ? 'lte' : 'lt';
+					$range_filter[ "date_terms.{$param}" ] = array();
+					$range_filter[ "date_terms.{$param}" ][ $range ] = $value;
+					$filter_parts['range'] = array( $range_filter );
 				}
 				//@todo support other compare operators
 			}
 
 			if ( ! empty( $date_terms ) ) {
-				$filter_parts['date_terms'] = $date_terms;
+				$filter_parts['date_terms'] = array(
+					"{$bool_check}" => $date_terms
+				);
 			}
 		}
 		echo '<pre>';
-		var_dump($filter_parts);
+		var_dump( $filter_parts );
 		echo '</pre>';
+
 		return $filter_parts;
 	}
 }
