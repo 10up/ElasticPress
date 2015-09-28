4869e91aa8c24005aaa6345d1d71e5425a288a53
John P Bloch <johnpbloch@gmail.com>
2015-09-27 19:32:44 -0500

    Implement a few interface methods

--- classes/class-ep-abstract-object-index.php
+++ classes/class-ep-abstract-object-index.php
@@ -4,4 +4,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 abstract class EP_Abstract_Object_Index implements EP_Object_Index {
+
+	protected $name = '';
+
+	public function __construct( $name ) {
+		$this->name = $name;
+	}
+
+	public function get_name() {
+		return $this->name;
+	}
+
+	public function set_name( $name ) {
+		$this->name = $name;
+	}
+
+
 }
