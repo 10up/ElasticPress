5293c2aed43087e9ec331a3faa22713613046242
John P Bloch <johnpbloch@gmail.com>
2015-09-27 19:32:08 -0500

    Add some methods to the interface

--- classes/interface-ep-object-index.php
+++ classes/interface-ep-object-index.php
@@ -4,4 +4,64 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 interface EP_Object_Index {
+
+	/**
+	 * Get the object type name
+	 *
+	 * @return string
+	 */
+	public function get_name();
+
+	/**
+	 * Set the object type name
+	 *
+	 * @param string $name
+	 */
+	public function set_name( $name );
+
+	/**
+	 * Get the settings needed by this type's mapping
+	 *
+	 * @return array
+	 */
+	public function get_settings();
+
+	/**
+	 * Get the mapping for this type
+	 *
+	 * @return array
+	 */
+	public function get_mappings();
+
+	/**
+	 * Index a document of this type
+	 *
+	 * Returns the response body if available, false otherwise.
+	 *
+	 * @param array $object The object data
+	 *
+	 * @return array|bool The response body if available, otherwise false
+	 */
+	public function index_document( $object );
+
+	/**
+	 * Get a document from ES
+	 *
+	 * Returns the document data if successful, false otherwise.
+	 *
+	 * @param mixed $object An object identifier
+	 *
+	 * @return array|bool The document data or false on failur
+	 */
+	public function get_document( $object );
+
+	/**
+	 * Delete a document from ES
+	 *
+	 * @param mixed $object An object identifier
+	 *
+	 * @return bool True for success, false for failure
+	 */
+	public function delete_document( $object );
+
 }
