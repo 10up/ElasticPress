0dfd096c746e7dca4416a78d36e539e4aa732890
John P Bloch <johnpbloch@gmail.com>
2015-09-28 17:14:16 -0500

    Copy over prepare_post() contents into the prepare_object() method

--- classes/class-ep-post-index.php
+++ classes/class-ep-post-index.php
@@ -35,7 +35,86 @@ class EP_Post_Index extends EP_Abstract_Object_Index {
 	 * @return array
 	 */
 	public function prepare_object( $object ) {
-		// TODO: Implement prepare_object() method.
+		$post_id = $this->get_object_identifier( $object );
+		$post    = get_post( $post_id );
+
+		$user = get_userdata( $post->post_author );
+
+		if ( $user instanceof WP_User ) {
+			$user_data = array(
+				'raw'          => $user->user_login,
+				'login'        => $user->user_login,
+				'display_name' => $user->display_name,
+				'id'           => $user->ID,
+			);
+		} else {
+			$user_data = array(
+				'raw'          => '',
+				'login'        => '',
+				'display_name' => '',
+				'id'           => '',
+			);
+		}
+
+		$post_date         = $post->post_date;
+		$post_date_gmt     = $post->post_date_gmt;
+		$post_modified     = $post->post_modified;
+		$post_modified_gmt = $post->post_modified_gmt;
+		$comment_count     = absint( $post->comment_count );
+		$comment_status    = absint( $post->comment_status );
+		$ping_status       = absint( $post->ping_status );
+		$menu_order        = absint( $post->menu_order );
+
+		if ( apply_filters( 'ep_ignore_invalid_dates', true, $post_id, $post ) ) {
+			if ( ! strtotime( $post_date ) || $post_date === "0000-00-00 00:00:00" ) {
+				$post_date = null;
+			}
+
+			if ( ! strtotime( $post_date_gmt ) || $post_date_gmt === "0000-00-00 00:00:00" ) {
+				$post_date_gmt = null;
+			}
+
+			if ( ! strtotime( $post_modified ) || $post_modified === "0000-00-00 00:00:00" ) {
+				$post_modified = null;
+			}
+
+			if ( ! strtotime( $post_modified_gmt ) || $post_modified_gmt === "0000-00-00 00:00:00" ) {
+				$post_modified_gmt = null;
+			}
+		}
+
+		$post_args = array(
+			'post_id'           => $post_id,
+			'post_author'       => $user_data,
+			'post_date'         => $post_date,
+			'post_date_gmt'     => $post_date_gmt,
+			'post_title'        => get_the_title( $post_id ),
+			'post_excerpt'      => $post->post_excerpt,
+			'post_content'      => apply_filters( 'the_content', $post->post_content ),
+			'post_status'       => $post->post_status,
+			'post_name'         => $post->post_name,
+			'post_modified'     => $post_modified,
+			'post_modified_gmt' => $post_modified_gmt,
+			'post_parent'       => $post->post_parent,
+			'post_type'         => $post->post_type,
+			'post_mime_type'    => $post->post_mime_type,
+			'permalink'         => get_permalink( $post_id ),
+			'terms'             => $this->prepare_terms( $post ),
+			'post_meta'         => $this->prepare_meta( $post ),
+			'date_terms'        => $this->prepare_date_terms( $post_date ),
+			'comment_count'     => $comment_count,
+			'comment_status'    => $comment_status,
+			'ping_status'       => $ping_status,
+			'menu_order'        => $menu_order,
+			'guid'              => $post->guid
+		);
+
+		/**
+		 * This filter is named poorly but has to stay to keep backwards compat
+		 */
+		$post_args = apply_filters( 'ep_post_sync_args', $post_args, $post_id );
+
+		return $post_args;
 	}
 
 	/**
