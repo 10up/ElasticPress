d83688b2b94eba11bffb52186ac362d4ce4c0730
John P Bloch <johnpbloch@gmail.com>
2015-09-30 16:10:02 -0500

    Add a user index class

---
+++ classes/class-ep-user-index.php
@@ -0,0 +1,60 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // exit if accessed directly
+}
+
+/**
+ * Class EP_User_Index
+ *
+ * @since 1.7.0
+ */
+class EP_User_Index extends EP_Abstract_Object_Index {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected $name = 'user';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function sync_setup() {
+		// TODO: Implement sync_setup() method.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function sync_teardown() {
+		// TODO: Implement sync_teardown() method.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_settings() {
+		// TODO: Implement get_settings() method.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_mappings() {
+		// TODO: Implement get_mappings() method.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function prepare_object( $object ) {
+		// TODO: Implement prepare_object() method.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function get_object_identifier( $object ) {
+		// TODO: Implement get_object_identifier() method.
+	}
+
+}
