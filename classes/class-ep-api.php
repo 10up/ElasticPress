94003a92ec248046b2841ea81e75e7cb3bc62245
Scott Kingsley Clark <lol@scottkclark.com>
2015-10-24 01:39:24 -0500

    Tweak to like for backwards compat
    Conflicts:
    	classes/class-ep-api.php

--- classes/class-ep-api.php
+++ classes/class-ep-api.php
@@ -1061,6 +1061,8 @@ class EP_API {
 						$meta_key_path = 'meta.' . $single_meta_query['key'];
 					} elseif ( in_array( $compare, array( '>=', '<=', '>', '<' ) ) ) {
 						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.double';
+					} elseif ( in_array( $compare, array( '=', '!=' ) ) ) {
+						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.raw';
 					} elseif ( $type && isset( $meta_query_type_mapping[ $type ] ) ) {
 						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.' . $meta_query_type_mapping[ $type ];
 					} else {
