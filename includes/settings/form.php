151ce118093038664501d6dd3e1a0639ca789ce7
Chris Wiegman <chris.wiegman@10up.com>
2015-12-01 12:31:38 -0500

    Make sure setup button works.

--- includes/settings/form.php
+++ includes/settings/form.php
@@ -25,9 +25,7 @@ if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
 	settings_fields( 'elasticpress' );
 	do_settings_sections( 'elasticpress' );
 
-	$stats = ep_get_index_status();
-
-	if ( ( $stats['status'] && ! is_wp_error( ep_check_host() ) ) || is_wp_error( ep_check_host() ) || get_site_option( 'ep_host' ) ) {
+	if ( ( ! ep_host_by_option() && ! is_wp_error( ep_check_host() ) ) || is_wp_error( ep_check_host() ) || get_site_option( 'ep_host' ) ) {
 		submit_button();
 	}
 
