31610c03727a3a333bcc16884e23f24161b62fbc
John P Bloch <johnpbloch@gmail.com>
2016-01-08 20:56:20 -0600

    Move name definition for mocks into the helper method

--- tests/test-ep-object-manager.php
+++ tests/test-ep-object-manager.php
@@ -14,21 +14,18 @@ class Test_EP_Object_Manager extends EP_Test_Base {
 	}
 
 	public function testRegisterObject() {
-		$object_index = $this->getObjectIndexMock();
-		$object_index
-			->expects( $this->any() )
-			->method( 'get_name' )
-			->will( $this->returnValue( 'foobar' ) );
+		$object_index = $this->getObjectIndexMock( 'foobar' );
 		$object_index
 			->expects( $this->once() )
 			->method( 'sync_setup' );
+		/** @var EP_Object_Index $object_index */
 		$this->manager->register_object($object_index);
 	}
 
 	/**
 	 * @return PHPUnit_Framework_MockObject_MockObject
 	 */
-	private function getObjectIndexMock() {
+	private function getObjectIndexMock( $name ) {
 		$object_index = $this->getMock(
 			'EP_Object_Index',
 			array(
@@ -47,6 +44,10 @@ class Test_EP_Object_Manager extends EP_Test_Base {
 				'bulk_index',
 			)
 		);
+		$object_index
+			->expects( $this->any() )
+			->method( 'get_name' )
+			->will( $this->returnValue( $name ) );
 
 		return $object_index;
 	}
