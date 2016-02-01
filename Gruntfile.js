f0b18cebcf4d86b639241ae2aeef5904bc104a7d
Darin Kotter <darin.kotter@my.eitc.edu>
2016-02-01 11:21:04 -0700

    Update POT file, switch to using grunt-wp-i18n to generate this.

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,34 +78,16 @@ module.exports = function ( grunt ) {
 
 			},
 
-			pot : {
-
-				options : {
-					text_domain : 'elasticpress',
-					dest        : 'lang/',
-					keywords    : [ //WordPress localisation functions
-						'__:1',
-						'_e:1',
-						'_x:1,2c',
-						'esc_html__:1',
-						'esc_html_e:1',
-						'esc_html_x:1,2c',
-						'esc_attr__:1',
-						'esc_attr_e:1',
-						'esc_attr_x:1,2c',
-						'_ex:1,2c',
-						'_n:1,2',
-						'_nx:1,2,4c',
-						'_n_noop:1,2',
-						'_nx_noop:1,2,3c'
-					]
-				},
-
-				files : {
-					src    : ['**/*.php'], //Parse all php files
-					expand : true
+			makepot: {
+				main: {
+					options: {
+						domainPath: 'lang',
+						mainFile: 'elasticpress.php',
+						potFilename: 'elasticpress.pot',
+						type: 'wp-plugin',
+						potHeaders: true
+					}
 				}
-
 			},
 
 			watch : {
@@ -140,6 +122,6 @@ module.exports = function ( grunt ) {
 	);
 
 	// A very basic default task.
-	grunt.registerTask ( 'default', ['uglify:production', 'sass', 'autoprefixer', 'cssmin', 'pot'] );
+	grunt.registerTask ( 'default', ['uglify:production', 'sass', 'autoprefixer', 'cssmin', 'makepot'] );
 
 };
