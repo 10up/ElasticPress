03a6b0f8560d37aa9bef1227aaf315a7af6e1eae
Scott Kingsley Clark <lol@scottkclark.com>
2016-02-11 01:26:56 -0600

    Fix order by of empty string

--- tests/test-single-site.php
+++ tests/test-single-site.php
@@ -1448,10 +1448,10 @@ class EPTestSingleSite extends EP_Test_Base {
 
 		$this->assertEquals( 4, $query->post_count );
 		$this->assertEquals( 4, $query->found_posts );
-		$this->assertEquals( $posts[0][0]['post_title'], $query->posts[0]->post_title );
-		$this->assertEquals( $posts[1][0]['post_title'], $query->posts[1]->post_title );
-		$this->assertEquals( $posts[2][0]['post_title'], $query->posts[2]->post_title );
-		$this->assertEquals( $posts[4][0]['post_title'], $query->posts[3]->post_title );
+		$this->assertEquals( $posts[4][0]['post_title'], $query->posts[0]->post_title );
+		$this->assertEquals( $posts[0][0]['post_title'], $query->posts[1]->post_title );
+		$this->assertEquals( $posts[1][0]['post_title'], $query->posts[2]->post_title );
+		$this->assertEquals( $posts[2][0]['post_title'], $query->posts[3]->post_title );
 
 	}
 
@@ -1500,10 +1500,10 @@ class EPTestSingleSite extends EP_Test_Base {
 
 		$this->assertEquals( 4, $query->post_count );
 		$this->assertEquals( 4, $query->found_posts );
-		$this->assertEquals( $posts[4][0]['post_title'], $query->posts[0]->post_title );
-		$this->assertEquals( $posts[2][0]['post_title'], $query->posts[1]->post_title );
-		$this->assertEquals( $posts[1][0]['post_title'], $query->posts[2]->post_title );
-		$this->assertEquals( $posts[0][0]['post_title'], $query->posts[3]->post_title );
+		$this->assertEquals( $posts[2][0]['post_title'], $query->posts[0]->post_title );
+		$this->assertEquals( $posts[1][0]['post_title'], $query->posts[1]->post_title );
+		$this->assertEquals( $posts[0][0]['post_title'], $query->posts[2]->post_title );
+		$this->assertEquals( $posts[4][0]['post_title'], $query->posts[3]->post_title );
 
 	}
 
