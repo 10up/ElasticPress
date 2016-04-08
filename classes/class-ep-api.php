d49167a28095e75ae991ccd91e0e993a8de6666b
Matt McAchran <matt.mcachran@webdevstudios.com>
2016-04-08 09:52:41 -0400

    fix meta queries

--- classes/class-ep-api.php
+++ classes/class-ep-api.php
@@ -1103,18 +1103,18 @@ class EP_API {
 
 					// Comparisons need to look at different paths
 					if ( in_array( $compare, array( 'exists', 'not exists' ) ) ) {
-						$meta_key_path = 'meta.' . $single_meta_query['key'];
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'];
 					} elseif ( in_array( $compare, array( '=', '!=' ) ) && ! $type ) {
-						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.raw';
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'] . '.raw';
 					} elseif ( 'like' === $compare ) {
-						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.value';
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'] . '.value';
 					} elseif ( $type && isset( $meta_query_type_mapping[ $type ] ) ) {
 						// Map specific meta field types to different Elasticsearch core types
-						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.' . $meta_query_type_mapping[ $type ];
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'] . '.' . $meta_query_type_mapping[ $type ];
 					} elseif ( in_array( $compare, array( '>=', '<=', '>', '<' ) ) ) {
-						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.double';
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'] . '.double';
 					} else {
-						$meta_key_path = 'meta.' . $single_meta_query['key'] . '.raw';
+						$meta_key_path = 'post_meta.' . $single_meta_query['key'] . '.raw';
 					}
 
 					switch ( $compare ) {
