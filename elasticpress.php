acbd4c4b87fbbf4f9f4ca97b2da50ab547457858
Taylor Lovett <tlovett88@gmail.com>
2016-08-02 10:03:59 -0400

    Remove events calendar for now

--- elasticpress.php
+++ elasticpress.php
@@ -47,7 +47,6 @@ require_once( 'modules/search/search.php' );
 require_once( 'modules/related-posts/related-posts.php' );
 require_once( 'modules/admin/admin.php' );
 require_once( 'modules/woocommerce/woocommerce.php' );
-require_once( 'modules/the-events-calendar/the-events-calendar.php' );
 require_once( 'modules/bbpress/bbpress.php' );
 
 /**
