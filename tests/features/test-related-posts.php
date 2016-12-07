73c485f1107c79b66dd48ed3de1b36ad238a6fb8
Taylor Lovett <tlovett88@gmail.com>
2016-12-07 19:09:48 +0000

    Clean up related posts tests

--- tests/features/test-related-posts.php
+++ tests/features/test-related-posts.php
@@ -31,7 +31,6 @@ class EPTestRelatedPostsFeature extends EP_Test_Base {
 	 * Clean up after each test. Reset our mocks
 	 *
 	 * @since 2.1
-	 * @group related_posts
 	 */
 	public function tearDown() {
 		parent::tearDown();
@@ -45,88 +44,15 @@ class EPTestRelatedPostsFeature extends EP_Test_Base {
 	 * Log action usage for tests
 	 *
 	 * @since  2.1
-	 * @group related_posts
 	 */
 	public function action_ep_related_html_attached() {
 		$this->fired_actions['ep_related_html_attached'] = true;
 	}
-
-	/**
-	 * Test that related posts is off
-	 *
-	 * @since 2.1
-	 * @group related_posts
-	 */
-	public function testRelatedPostsOff() {
-		delete_option( 'ep_active_features' );
-
-		$post_ids = array();
-
-		ep_create_and_sync_post();
-
-		ep_refresh_index();
-
-		add_action( 'ep_related_html_attached', array( $this, 'action_ep_related_html_attached' ), 10, 0 );
-
-		$args = array(
-			'ep_integrate' => true,
-		);
-
-		$query = new WP_Query( $args );
-
-		ob_start();
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-
-				the_content();
-			}
-		}
-		ob_get_clean();
-
-		$this->assertTrue( empty( $this->fired_actions['ep_related_html_attached'] ) );
-	}
-
-	/**
-	 * Test that related posts is on
-	 *
-	 * @since 2.1
-	 * @group related_posts
-	 */
-	public function testRelatedPostsOn() {
-		$post_ids = array();
-
-		ep_create_and_sync_post();
-
-		ep_refresh_index();
-
-		ep_activate_feature( 'related_posts' );
-
-		EP_Features::factory()->setup_features();
-
-		add_action( 'ep_related_html_attached', array( $this, 'action_ep_related_html_attached' ), 10 );
-
-		$args = array(
-			'ep_integrate' => true,
-		);
-
-		$query = new WP_Query( $args );
-
-		ob_start();
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-
-				the_content();
-			}
-		}
-		ob_get_clean();
-
-		$this->assertTrue( ! empty( $this->fired_actions['ep_related_html_attached'] ) );
-	}
 	
 	/**
 	 * Test for related post args filter
+	 *
+	 * @group related_posts
 	 */
 	public function testFindRelatedPostFilter(){
 		$post_id = ep_create_and_sync_post( array( 'post_content' => 'findme test 1' ) );
@@ -143,14 +69,12 @@ class EPTestRelatedPostsFeature extends EP_Test_Base {
 		$related = ep_find_related( $post_id );
 		$this->assertEquals( 2, sizeof( $related ) );
 		remove_filter( 'ep_find_related_args', array( $this, 'find_related_posts_filter' ) );
-		
-		$related = ep_find_related( $post_id );
-		$this->assertEquals( 3, sizeof( $related ) );
 	}
 	
 	/**
+	 * Detect EP fire
+	 * 
 	 * @param $args
-	 *
 	 * @return mixed
 	 */
 	public function find_related_posts_filter( $args ){
