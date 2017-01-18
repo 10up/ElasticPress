a5cac51cae5a6dc42a9b9e200d0852274ae08d1f
Taylor Lovett <tlovett88@gmail.com>
2017-01-18 09:38:15 -0500

    Dont dereference query argument. Closes #682

--- classes/class-ep-wp-query-integration.php
+++ classes/class-ep-wp-query-integration.php
@@ -159,10 +159,10 @@ class EP_WP_Query_Integration {
 	 * Filter the posts array to contain ES query results in EP_Post form. Pull previously queried posts.
 	 *
 	 * @param array $posts
-	 * @param object &$query
+	 * @param object $query
 	 * @return array
 	 */
-	public function filter_the_posts( $posts, &$query ) {
+	public function filter_the_posts( $posts, $query ) {
 		if ( ! ep_elasticpress_enabled( $query ) || apply_filters( 'ep_skip_query_integration', false, $query ) || ! isset( $this->posts_by_query[spl_object_hash( $query )] ) ) {
 			return $posts;
 		}
