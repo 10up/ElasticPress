38534779227e3ee586f3e09ceac9f2fc8db5776b
Allan Collins <allan.collins@10up.com>
2017-03-08 10:57:58 -0700

    Index Prefixing.

--- classes/class-ep-config.php
+++ classes/class-ep-config.php
@@ -125,6 +125,10 @@ class EP_Config {
 			$index_name = false;
 		}
 
+		if ( defined( 'EP_INDEX_PREFIX' ) && EP_INDEX_PREFIX ) {
+			$index_name = EP_INDEX_PREFIX . $index_name;
+		}
+
 		return apply_filters( 'ep_index_name', $index_name, $blog_id );
 	}
 
@@ -162,6 +166,10 @@ class EP_Config {
 		$slug = preg_replace( '#[^\w]#', '', $slug );
 
 		$alias = $slug . '-global';
+
+		if ( defined( 'EP_INDEX_PREFIX' ) && EP_INDEX_PREFIX ) {
+			$alias = EP_INDEX_PREFIX . $alias;
+		}
 
 		return apply_filters( 'ep_global_alias', $alias );
 	}
