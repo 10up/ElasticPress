1ed3d1dac6c8537526d9adcd2bc1b453906ed4ed
Ivan Kristianto <ivan.kristianto@gmail.com>
2017-05-08 14:03:51 +0700

    remove duplicate woocommerce_shop_order_search_fields filter

--- features/woocommerce/woocommerce.php
+++ features/woocommerce/woocommerce.php
@@ -573,7 +573,6 @@ function ep_wc_search_order( $wp ){
 		//we found the order. don't query ES
 		unset( $wp->query_vars['s'] );
 		$wp->query_vars['post__in'] = array( $order->get_id() );
-		$wp->query_vars['ep_integrate'] = true;
 	}
 }
 
@@ -594,7 +593,6 @@ function ep_wc_setup() {
 		add_filter( 'ep_post_sync_args_post_prepare_meta', 'ep_wc_remove_legacy_meta', 10, 2 );
 		add_action( 'pre_get_posts', 'ep_wc_translate_args', 11, 1 );
 
-		add_filter( 'woocommerce_shop_order_search_fields', '__return_empty_array' );
 		add_action( 'parse_query', 'ep_wc_search_order', 11, 1 );
 	}
 }
