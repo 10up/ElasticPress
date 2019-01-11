9fc4f610f007d1e9a2e98df4e9003656fd2703ce
david <david.chandra@10up.com>
2019-01-11 17:42:18 +0700

    Add elasticsearch version in settings.

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -46,6 +46,17 @@ if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
 					<?php endif; ?>
 				</td>
 			</tr>
+			<tr>
+				<th scope="row">
+					<label for="ep_host"><?php esc_html_e( 'Elasticsearch Version', 'elasticpress' ); ?></label></th>
+				<td>
+					<?php if ( $version = ep_get_elasticsearch_version() ) : ?>
+						<span class="description"><?php echo esc_html( $version ); ?></span>
+					<?php else : ?>
+						<span class="description">&mdash;</span>
+					<?php endif; ?>
+				</td>
+			</tr>
 			<?php
 			if ( ep_is_epio() && current_user_can( 'manage_options' ) ) {
 				$credentials = ep_get_epio_credentials();
