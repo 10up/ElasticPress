2be24d17694a5ccee57d3591fbb73e26c36b124f
Taylor Lovett <tlovett88@gmail.com>
2019-02-10 00:03:50 -0500

    MERGE: Wildcard match_phrase

--- includes/classes/Indexable.php
+++ includes/classes/Indexable.php
@@ -615,10 +615,8 @@ abstract class Indexable {
 					case 'like':
 						if ( isset( $single_meta_query['value'] ) ) {
 							$terms_obj = array(
-								'query' => array(
-									'match' => array(
-										$meta_key_path => $single_meta_query['value'],
-									),
+								'match_phrase' => array(
+									$meta_key_path => $single_meta_query['value'],
 								),
 							);
 						}
