6e916a3910d96b4ffead6fffe675c602b88eb3d4
Taylor Lovett <tlovett88@gmail.com>
2019-10-20 23:22:31 -0400

    Fix wpa script

--- run-wpacceptance.sh
+++ run-wpacceptance.sh
@@ -1,14 +1,14 @@
 for i in 1 2 3; do
-  echo -e "\033[32mRunning wp-acceptance with snapshot $1\033[0m"
-  ./vendor/bin/wpacceptance run --snapshot_id=$1
-  EXIT_CODE=$?
+    ./vendor/bin/wpacceptance run
 
-  if [ $EXIT_CODE -gt 1 ]; then
-    echo "Retrying..."
-    sleep 3
-  else
-    break
-  fi
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -gt 1 ]; then
+        echo "Retrying..."
+        sleep 3
+    else
+        break
+    fi
 done
 
 exit $EXIT_CODE
