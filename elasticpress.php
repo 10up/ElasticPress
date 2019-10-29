6720cef374bc79f29e532969559657499eb51142
Darin Kotter <darin.kotter@my.eitc.edu>
2019-10-29 09:27:42 -0600

    Remove comment

--- elasticpress.php
+++ elasticpress.php
@@ -126,7 +126,6 @@ if ( version_compare( $wp_version, '5.1', '>=' ) || 0 === stripos( $wp_version, 
 	);
 }
 
-// TODO: update version number
 if ( version_compare( $wp_version, '5.3', '>=' ) || 0 === stripos( $wp_version, '5.3-' ) ) {
 	Features::factory()->register_feature(
 		new Feature\Terms\Terms()
