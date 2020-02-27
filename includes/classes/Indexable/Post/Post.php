95cd34ee8abd1cdcbc38c94019e262bdce321952
Nick Daugherty <ndaugherty987@gmail.com>
2020-02-27 15:17:57 -0700

    Revert "Revert "Split out the generation of indexable taxonomies list""
    This reverts commit 73ba897866f6beb5d6335f6b1b47d306c6f464d0.

--- includes/classes/Indexable/Post/Post.php
+++ includes/classes/Indexable/Post/Post.php
@@ -357,13 +357,12 @@ class Post extends Indexable {
 	}
 
 	/**
-	 * Prepare terms to send to ES.
-	 *
+	 * Get an array of taxonomies that are indexable for the given post
+	 * 
 	 * @param WP_Post $post Post object
-	 * @since 0.1.0
-	 * @return array
+	 * @return array Array of taxonomy slugs that should be indexed
 	 */
-	private function prepare_terms( $post ) {
+	public function get_indexable_post_taxonomies( $post ) {
 		$taxonomies          = get_object_taxonomies( $post->post_type, 'objects' );
 		$selected_taxonomies = [];
 
@@ -372,7 +371,7 @@ class Post extends Indexable {
 				$selected_taxonomies[] = $taxonomy;
 			}
 		}
-
+		
 		/**
 		 * Filter taxonomies to be synced with post
 		 *
@@ -382,6 +381,19 @@ class Post extends Indexable {
 		 * @return  {array} New taxonomies
 		 */
 		$selected_taxonomies = apply_filters( 'ep_sync_taxonomies', $selected_taxonomies, $post );
+
+		return $selected_taxonomies;
+	}
+
+	/**
+	 * Prepare terms to send to ES.
+	 *
+	 * @param WP_Post $post Post object
+	 * @since 0.1.0
+	 * @return array
+	 */
+	private function prepare_terms( $post ) {
+		$selected_taxonomies = $this->get_indexable_post_taxonomies( $post );
 
 		if ( empty( $selected_taxonomies ) ) {
 			return [];
