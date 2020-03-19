9f0fb0a7d7ace0b9b5085f135369fe55a56da31b
Ashar Irfan <mrasharirfan@gmail.com>
2020-03-19 16:46:29 +0500

    New WP Acceptance test for testing user meta sync

--- tests/wpa/indexables/UserTest.php
+++ tests/wpa/indexables/UserTest.php
@@ -11,7 +11,25 @@
 class UserTest extends TestBase {
 
 	/**
-	 * Test a simple user sync
+	 * Search user by username.
+	 *
+	 * @param string                      $username Username.
+	 * @param \WPAcceptance\PHPUnit\Actor $actor    Current actor.
+	 */
+	public function searchUser( $username = '', \WPAcceptance\PHPUnit\Actor $actor ) {
+		$actor->moveTo( 'wp-admin/users.php' );
+
+		$actor->waitUntilElementVisible( '#user-search-input' );
+
+		$actor->typeInField( '#user-search-input', $username );
+
+		$actor->click( '#search-submit' );
+
+		$actor->waitUntilElementVisible( '.wp-list-table' );
+	}
+
+	/**
+	 * Test a simple user sync.
 	 *
 	 * @testdox I can successfully run a simple user sync.
 	 */
@@ -33,15 +51,7 @@ class UserTest extends TestBase {
 
 		$I->waitUntilElementContainsText( 'Sync complete', '.sync-status' );
 
-		$I->moveTo( 'wp-admin/users.php' );
-
-		$I->waitUntilElementVisible( '#user-search-input' );
-
-		$I->typeInField( '#user-search-input', 'testuser' );
-
-		$I->click( '#search-submit' );
-
-		$I->waitUntilElementVisible( '.wp-list-table' );
+		$this->searchUser( 'testuser', $I );
 
 		$I->seeText( 'testuser@example.com' );
 
@@ -58,5 +68,60 @@ class UserTest extends TestBase {
 		$I->seeText( '"total": 1', '.query-results' );
 
 		$I->seeText( '"user_email": "testuser@example.com"', '.query-results' );
+	}
+
+	/**
+	 * Test a simple user sync with meta.
+	 *
+	 * @testdox I can successfully run a simple user sync with meta.
+	 */
+	public function testUserMetaSync() {
+		$I = $this->openBrowserPage();
+
+		$I->loginAs( 'wpsnapshots' );
+
+		$this->searchUser( 'testuser', $I );
+
+		$I->seeText( 'testuser@example.com' );
+
+		$user_link = $I->getElement( '#the-list .column-username a' );
+
+		$I->click( $user_link );
+
+		$I->waitUntilElementVisible( '.form-table' );
+
+		$I->typeInField( '#first_name', 'John' );
+
+		$I->typeInField( '#last_name', 'Doe' );
+
+		$I->click( '#submit' );
+
+		$I->moveTo( 'wp-admin/admin.php?page=elasticpress' );
+
+		$I->click( '.start-sync' );
+
+		$I->waitUntilElementContainsText( 'Sync complete', '.sync-status' );
+
+		$this->searchUser( 'testuser', $I );
+
+		$I->seeText( 'testuser@example.com' );
+
+		$I->click( '#wp-admin-bar-debug-bar' );
+
+		$I->click( '#debug-menu-link-EP_Debug_Bar_ElasticPress' );
+
+		$I->seeText( 'Query Response Code: HTTP 200' );
+
+		$I->waitUntilElementVisible( '.query-result-toggle' );
+
+		$I->click( '.query-result-toggle' );
+
+		$I->seeText( '"total": 1', '.query-results' );
+
+		$I->seeText( '"user_email": "testuser@example.com"', '.query-results' );
+
+		$I->seeText( '"value": "John"', '.query-results' );
+
+		$I->seeText( '"value": "Doe"', '.query-results' );
 	}
 }
