39eb350cb7fd84e9770602c521af540c99e625a1
Nick Daugherty <ndaugherty987@gmail.com>
2020-03-24 16:55:07 -0600

    Scaffold for tests

---
+++ tests/php/indexables/Post/SyncManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Test post SyncManager functionality
+ *
+ * @package elasticpress
+ */
+
+namespace ElasticPressTest\Indexables\Post;
+
+use ElasticPress;
+
+/**
+ * Test post indexable class
+ */
+class TestSyncManager extends BaseTestCase {
+	public function testPermissionCheckBypassFilter() {
+		
+	}
+}
