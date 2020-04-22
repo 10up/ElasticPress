1c844097bc1fd63c5cfc272d06f47d57d5a22487
Tung Du <dinhtungdu@gmail.com>
2020-04-22 10:05:31 +0700

    test: setup test file

---
+++ tests/php/features/TestSearchOrdering.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Test search feature
+ *
+ * @package elasticpress
+ */
+
+namespace ElasticPressTest;
+
+use ElasticPress;
+
+/**
+ * Search test class
+ */
+class TestSearchOrdering extends BaseTestCase {
+
+	/**
+	 * Setup each test.
+	 *
+	 * @since 2.1
+	 */
+	public function setUp() {
+		global $wpdb;
+		parent::setUp();
+		$wpdb->suppress_errors();
+
+		$admin_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
+
+		wp_set_current_user( $admin_id );
+
+		ElasticPress\Elasticsearch::factory()->delete_all_indices();
+		ElasticPress\Indexables::factory()->get( 'post' )->put_mapping();
+
+		ElasticPress\Indexables::factory()->get( 'post' )->sync_manager->sync_queue = [];
+
+		$this->setup_test_post_type();
+		ElasticPress\Features::factory()->activate_feature( 'search' );
+		ElasticPress\Features::factory()->activate_feature( 'searchordering' );
+	}
+
+	/**
+	 * Clean up after each test. Reset our mocks
+	 *
+	 * @since 2.1
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		// make sure no one attached to this
+		remove_filter( 'ep_sync_terms_allow_hierarchy', array( $this, 'ep_allow_multiple_level_terms_sync' ), 100 );
+		$this->fired_actions = array();
+	}
+
+	/**
+	 * @return weighting sub-feature
+	 */
+	public function get_feature() {
+		return ElasticPress\Features::factory()->get_registered_feature( 'searchordering' );
+	}
+
+}
