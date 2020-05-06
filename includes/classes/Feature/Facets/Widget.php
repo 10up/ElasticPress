044db59597a6d60c561faae03e6d4ec3cb573c91
Michael LaRoy <mike.laroy@gmail.com>
2020-05-06 12:54:25 -0700

    add rel="nofollow" to facet links

--- includes/classes/Feature/Facets/Widget.php
+++ includes/classes/Feature/Facets/Widget.php
@@ -171,7 +171,7 @@ class Widget extends WP_Widget {
 							}
 							?>
 							<div class="term selected level-<?php echo (int) $term->level; ?>" data-term-name="<?php echo esc_attr( strtolower( $term->name ) ); ?>" data-term-slug="<?php echo esc_attr( strtolower( $term_slug ) ); ?>">
-								<a href="<?php echo esc_attr( $feature->build_query_url( $new_filters ) ); ?>">
+								<a href="<?php echo esc_attr( $feature->build_query_url( $new_filters ) ); ?>" rel="nofollow">
 									<input type="checkbox" checked>
 									<?php echo esc_html( $term->name ); ?>
 								</a>
@@ -234,7 +234,7 @@ class Widget extends WP_Widget {
 								}
 								?>
 								<div class="term <?php if ( empty( $term->count ) ) : ?>empty-term<?php endif; ?> <?php if ( $selected ) : ?>selected<?php endif; ?> level-<?php echo (int) $term->level; ?>" data-term-name="<?php echo esc_attr( strtolower( $term->name ) ); ?>" data-term-slug="<?php echo esc_attr( strtolower( $term->slug ) ); ?>">
-									<a href="<?php echo esc_attr( $feature->build_query_url( $new_filters ) ); ?>">
+									<a href="<?php echo esc_attr( $feature->build_query_url( $new_filters ) ); ?>" rel="nofollow">
 										<input type="checkbox" <?php if ( $selected ) : ?>checked<?php endif; ?>>
 										<?php echo esc_html( $term->name ); ?>
 									</a>
