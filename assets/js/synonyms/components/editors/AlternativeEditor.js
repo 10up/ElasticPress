469bd27fc7ef9ab2b59806d5b17ca2e3925fc557
Christian Chung <christian.m.chung@gmail.com>
2020-08-13 18:24:16 -0400

    Prevent enter on primary term

--- assets/js/synonyms/components/editors/AlternativeEditor.js
+++ assets/js/synonyms/components/editors/AlternativeEditor.js
@@ -28,6 +28,20 @@ export default function AlternativeEditor(props) {
 		};
 	};
 
+	/**
+	 * Handle key down.
+	 *
+	 * @param {React.SyntheticEvent} event Keydown event.
+	 */
+	const handleKeyDown = (event) => {
+		switch (event.key) {
+			case 'Enter':
+				event.preventDefault();
+				break;
+			default:
+		}
+	};
+
 	useEffect(() => {
 		dispatch({
 			type: 'UPDATE_ALTERNATIVE_PRIMARY',
@@ -46,6 +60,7 @@ export default function AlternativeEditor(props) {
 				className="ep-synonyms__input"
 				onChange={(e) => setPrimaryTerm(e.target.value)}
 				value={primaryTerm}
+				onKeyDown={handleKeyDown}
 				ref={primaryRef}
 			/>
 			<LinkedMultiInput
