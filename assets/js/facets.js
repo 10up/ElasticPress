d0f8fc359c515d7d40bb645beb8f3affb9184e8d
Jeffrey Paul <jeffpaul@hotmail.com>
2021-02-03 13:41:04 -0600

    cleanup cherry-pick commits
    Co-Authored-By: Johannes Kinast <5927337+goaround@users.noreply.github.com>

--- assets/js/facets.js
+++ assets/js/facets.js
@@ -6,15 +6,7 @@ import { debounce } from './utils/helpers';
  * by the ep_facet_search_threshold filter
  *
  * @param {event} event - keyup
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @param {Node} facetTerms - terms node
-=======
- * @param facetTerms
->>>>>>> d7acb690 (Update facets.js)
-=======
- * @param {Node} facetTerms - terms node
->>>>>>> c12f3d42 (Add facetTerms type and description)
  */
 const handleFacetSearch = (event, facetTerms) => {
 	const { target } = event;
@@ -37,7 +29,6 @@ const handleFacetSearch = (event, facetTerms) => {
  * Filter facet choices to match the search field term
  */
 const facets = document.querySelectorAll('.widget_ep-facet');
-<<<<<<< HEAD
 
 facets.forEach((facet) => {
 	const facetSearchInput = facet.querySelector('.facet-search');
@@ -47,28 +38,6 @@ facets.forEach((facet) => {
 		return;
 	}
 
-=======
-
-facets.forEach((facet) => {
-	const facetSearchInput = facet.querySelector('.facet-search');
-	const facetTerms = facet.querySelector('.terms');
-<<<<<<< HEAD
-	
-<<<<<<< HEAD
->>>>>>> 36143e7b (Support multiple Facets)
-=======
-	if(!facetSearchInput) {
-		return;
-	}
-	
->>>>>>> 61d3fb08 (Update facets.js)
-=======
-
-	if (!facetSearchInput) {
-		return;
-	}
-
->>>>>>> d7acb690 (Update facets.js)
 	facet.querySelector('.facet-search').addEventListener(
 		'keyup',
 		debounce((event) => {
@@ -77,15 +46,6 @@ facets.forEach((facet) => {
 			}
 
 			handleFacetSearch(event, facetTerms);
-<<<<<<< HEAD
-<<<<<<< HEAD
 		}, 200),
 	);
-=======
-		}, 200)
->>>>>>> 36143e7b (Support multiple Facets)
-=======
-		}, 200),
-	);
->>>>>>> d7acb690 (Update facets.js)
 });
