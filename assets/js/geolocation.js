ac69d56ffaa32b05e21fb6e2ff26d166c9a30e21
PypWalters <stephanie.walters@10up.com>
2021-02-19 17:28:13 -0500

    remove spaces around params

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -3,97 +3,97 @@ const { epgl } = window;
 /**
  * Append the hidden inputs so we can submit lat long with the search form
  */
-function appendHiddenInputs( savedLocation = [] ) {
-	const buttonHolders = document.querySelectorAll( epgl.selector );
-	const latInput = document.createElement( 'input' );
-	const longInput = document.createElement( 'input' );
+function appendHiddenInputs(savedLocation = []) {
+	const buttonHolders = document.querySelectorAll(epgl.selector);
+	const latInput = document.createElement('input');
+	const longInput = document.createElement('input');
 
-	latInput.setAttribute( 'type', 'hidden' );
-	latInput.setAttribute( 'name', 'epgl_latitude' );
-	longInput.setAttribute( 'type', 'hidden' );
-	longInput.setAttribute( 'name', 'epgl_longitude' );
+	latInput.setAttribute('type', 'hidden');
+	latInput.setAttribute('name', 'epgl_latitude');
+	longInput.setAttribute('type', 'hidden');
+	longInput.setAttribute('name', 'epgl_longitude');
 
-	if ( savedLocation ) {
+	if (savedLocation) {
 		latInput.value = savedLocation[0];
 		longInput.value = savedLocation[1];
 	}
 
-	buttonHolders.forEach( buttonHolder => {
-		buttonHolder.append( latInput );
-		buttonHolder.append( longInput );
-	} );
+	buttonHolders.forEach(buttonHolder => {
+		buttonHolder.append(latInput);
+		buttonHolder.append(longInput);
+	});
 }
 
 /**
  * Appends a simple message that the location is set
  */
 function appendLocationSetMessage() {
-	const buttonHolders  = document.querySelectorAll( epgl.selector );
-	const locatedWrapper = document.createElement( 'div' );
-	const message        = document.createElement( 'span' );
-	const removeButton   = document.createElement( 'button' );
+	const buttonHolders  = document.querySelectorAll(epgl.selector);
+	const locatedWrapper = document.createElement('div');
+	const message        = document.createElement('span');
+	const removeButton   = document.createElement('button');
 
-	locatedWrapper.classList.add( 'ep-located' );
+	locatedWrapper.classList.add('ep-located');
 
 	message.innerText = epgl.locationSetMessage;
 
 	removeButton.innerText = epgl.removeButtonText;
-	removeButton.addEventListener( 'click', clearCurrentLocation );
+	removeButton.addEventListener('click', clearCurrentLocation);
 
-	locatedWrapper.append( message );
-	locatedWrapper.append( removeButton );
+	locatedWrapper.append(message);
+	locatedWrapper.append(removeButton);
 
-	buttonHolders.forEach( buttonHolder => {
-		buttonHolder.append( locatedWrapper );
-	} );
+	buttonHolders.forEach(buttonHolder => {
+		buttonHolder.append(locatedWrapper);
+	});
 }
 
 /**
  * Remove the location set messaging
  */
 function removeLocationSetMessage() {
-	const messageWrappers = document.getElementsByClassName( 'ep-located' );
-	messageWrappers.forEach( messageWrapper => {
-		messageWrapper.querySelector( 'button' ).removeEventListener( 'click', clearCurrentLocation );
+	const messageWrappers = document.getElementsByClassName('ep-located');
+	messageWrappers.forEach(messageWrapper => {
+		messageWrapper.querySelector('button').removeEventListener('click', clearCurrentLocation);
 		messageWrapper.remove();
-	} );
+	});
 }
 
 /**
  * Appends the "Location Me" button to the specified selector
  */
 function appendLocationButton() {
-	const locationButton = document.createElement( 'button' );
-	const buttonHolders = document.querySelectorAll( epgl.selector );
+	const locationButton = document.createElement('button');
+	const buttonHolders = document.querySelectorAll(epgl.selector);
 
-	locationButton.classList.add( 'ep-locate-me' );
+	locationButton.classList.add('ep-locate-me');
 	locationButton.innerText = epgl.buttonText;
-	locationButton.addEventListener( 'click', setUserLocation );
+	locationButton.addEventListener('click', setUserLocation);
 
-	buttonHolders.forEach( buttonHolder => {
-		buttonHolder.append( locationButton );
-	} );
+	buttonHolders.forEach(buttonHolder => {
+		buttonHolder.append(locationButton);
+	});
 }
 
 /**
  * Remove Locate me button
  */
 function removeLocationButton() {
-	const locationButtons = document.getElementsByClassName( 'ep-locate-me' );
-	locationButtons.forEach( locationButton => {
-		locationButton.removeEventListener( 'click', setUserLocation );
+	const locationButtons = document.getElementsByClassName('ep-locate-me');
+	locationButtons.forEach(locationButton => {
+		locationButton.removeEventListener('click', setUserLocation);
 		locationButton.remove();
-	} );
+	});
 }
 
 /**
  * Clears the current location lat long information
  */
-function clearCurrentLocation( event ) {
+function clearCurrentLocation(event) {
 	event.preventDefault();
 
 	clearLocationCookie();
-	updateInputs( '', '' );
+	updateInputs('', '');
 	removeLocationSetMessage();
 	appendLocationButton();
 }
@@ -101,19 +101,19 @@ function clearCurrentLocation( event ) {
 /**
  * Get the user's location and save it in a cookie
  */
-function setUserLocation( event ) {
+function setUserLocation(event) {
 	event.preventDefault();
 
-	if ( navigator.geolocation ) {
-		navigator.geolocation.getCurrentPosition( function ( position ) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {
 			const lat = position.coords.latitude;
 			const long = position.coords.longitude;
 
-			setLocationCookie( lat, long );
-			updateInputs( lat, long );
+			setLocationCookie(lat, long);
+			updateInputs(lat, long);
 			removeLocationButton();
 			appendLocationSetMessage();
-		} );
+		});
 	} else {
 		epgl.selector.innerHTML = epgl.getLocationErrorMessage;
 	}
@@ -124,16 +124,16 @@ function setUserLocation( event ) {
  */
 function getLocationFromCookie() {
 	// Split cookie string and get all individual name=value pairs in an array
-	const cookieArr = document.cookie.split( ';' );
+	const cookieArr = document.cookie.split(';');
 	let coords =[];
 
 	// Loop through the array elements
-	for ( let i = 0; i < cookieArr.length; i++ ) {
-		const cookiePair = cookieArr[i].split( '=' );
+	for (let i = 0; i < cookieArr.length; i++) {
+		const cookiePair = cookieArr[i].split('=');
 
-		if ( 'epgl' == cookiePair[0].trim() ) {
-			coords = cookiePair[1].split( ',' );
-			if ( 2 === coords.length ) {
+		if ('epgl' == cookiePair[0].trim()) {
+			coords = cookiePair[1].split(',');
+			if (2 === coords.length) {
 				return [ coords[0], coords[1] ]; // lat, long
 			}
 		}
@@ -151,34 +151,34 @@ function clearLocationCookie() {
 /**
  * Set location Cookie
  */
-function setLocationCookie( lat, long ) {
+function setLocationCookie(lat, long) {
 	document.cookie = `epgl=${lat},${long}; expires=Tue, January 1, 2030 12:00:00 UTC`;
 }
 
 /**
  * Updates the hidden inputs with the set coordinates
  */
-function updateInputs( lat, long ) {
-	const latInputs = document.querySelectorAll( 'input[name="epgl_latitude"' );
-	const longInputs = document.querySelectorAll( 'input[name="epgl_longitude"' );
+function updateInputs(lat, long) {
+	const latInputs = document.querySelectorAll('input[name="epgl_latitude"');
+	const longInputs = document.querySelectorAll('input[name="epgl_longitude"');
 
-	latInputs.forEach( latInput => {
+	latInputs.forEach(latInput => {
 		latInput.value = lat;
-	} );
+	});
 
-	longInputs.forEach( longInput => {
+	longInputs.forEach(longInput => {
 		longInput.value = long;
-	} );
+	});
 }
 
-if ( epgl.selector && '' !== epgl.selector && null !== document.querySelector( epgl.selector ) ) {
+if (epgl.selector && '' !== epgl.selector && null !== document.querySelector(epgl.selector)) {
 	const savedLocation = getLocationFromCookie();
 
-	if ( savedLocation ) {
+	if (savedLocation) {
 		appendLocationSetMessage();
 	} else {
 		appendLocationButton();
 	}
 
-	appendHiddenInputs( savedLocation );
+	appendHiddenInputs(savedLocation);
 }
