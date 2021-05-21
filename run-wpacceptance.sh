df05d024e86f39a2f5575b0cc0e33910f55bccdf
Felipe Elia <felipe.elia@gmail.com>
2021-05-21 18:41:01 -0300

    Go with the regular echo

--- run-wpacceptance.sh
+++ run-wpacceptance.sh
@@ -93,14 +93,14 @@ fi
 
 # This variable will hold all tests failed with the "Page crashed" error.
 ERRORS=''
-TTY=$(tty)
 
 for i in $(seq 1 $ATTEMPTS); do
 
-  # tee is here to output it in "real time", rather than waiting until it is 100% and echoing it.
-  TEST_OUTPUT=$(./vendor/bin/wpacceptance run --cache_environment --screenshot_on_failure | tee $TTY)
+  TEST_OUTPUT=$(./vendor/bin/wpacceptance run --cache_environment --screenshot_on_failure)
 
   EXIT_CODE=$?
+
+  echo "${TEST_OUTPUT}"
 
   if [ $EXIT_CODE -ge 1 ] && [ $i -lt $ATTEMPTS ]; then
 
