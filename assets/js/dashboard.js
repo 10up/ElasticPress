360924af184ff1ae4e1c2efb820b5f335d67c7c1
Ramon <Rahmon@users.noreply.github.com>
2021-08-10 17:26:21 -0300

    Fix else block with only a if statement

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -158,23 +158,21 @@ if (epDash.index_meta) {
 			// Sync finished
 			syncStatus = 'finished';
 			updateSyncDash();
-		} else {
+		} else if (epDash.auto_start_index) {
 			// We are mid sync
-			if (epDash.auto_start_index) {
-				syncStatus = 'sync';
+			syncStatus = 'sync';
 
-				history.pushState(
-					{},
-					document.title,
-					document.location.pathname + document.location.search.replace(/&do_sync/, ''),
-				);
+			history.pushState(
+				{},
+				document.title,
+				document.location.pathname + document.location.search.replace(/&do_sync/, ''),
+			);
 
-				updateSyncDash();
-				sync();
-			} else {
-				syncStatus = 'pause';
-				updateSyncDash();
-			}
+			updateSyncDash();
+			sync();
+		} else {
+			syncStatus = 'pause';
+			updateSyncDash();
 		}
 	}
 } else {
