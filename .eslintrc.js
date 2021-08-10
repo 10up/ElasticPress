eaff090efb5d2b107e0581a77fe87e22838a9751
Ramon <Rahmon@users.noreply.github.com>
2021-08-10 16:03:31 -0300

    Remove custom rules in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,7 @@ const defaultEslintrc = require('10up-toolkit/config/.eslintrc');
 
 module.exports = {
 	...defaultEslintrc,
-	extends: '@10up/eslint-config',
-	plugins: ['react'],
 	globals: {
-		wp: true,
 		jQuery: true,
-	},
-	rules: {
-		'react/jsx-uses-react': 'error',
-		'react/jsx-uses-vars': 'error',
-		'no-console': [1],
-		'no-alert': [1],
-		camelcase: [1],
-		'no-debugger': [1],
-		'no-unused-vars': [1],
-		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-		'no-plusplus': [0],
-		'import/prefer-default-export': [0],
-		'no-lonely-if': [0],
-		'func-names': [0],
-	},
-	env: {
-		browser: true,
-	},
+	}
 };
