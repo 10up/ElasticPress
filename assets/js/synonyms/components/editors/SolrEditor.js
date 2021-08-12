0b02fd4d590234d202149c4b02888bec76da7a65
Ramon <Rahmon@users.noreply.github.com>
2021-08-12 19:49:21 -0300

    Remove unnecesary useEffect and useState from SolrEditor.js

--- assets/js/synonyms/components/editors/SolrEditor.js
+++ assets/js/synonyms/components/editors/SolrEditor.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { State, Dispatch } from '../../context';
 import { reduceStateToSolr } from '../../utils';
 
@@ -12,19 +12,7 @@ const SolrEditor = () => {
 	const dispatch = useContext(Dispatch);
 	const reducedState = reduceStateToSolr(state);
 	const { isSolrEditable, isSolrVisible } = state;
-	const [solr, setSolr] = useState(reducedState);
 	const { synonymsTextareaInputName, solrInputHeading } = window.epSynonyms.i18n;
-
-	useEffect(() => {
-		setSolr(reducedState);
-	}, [reducedState]);
-
-	useEffect(() => {
-		if (!isSolrEditable && solr !== reducedState) {
-			dispatch({ type: 'REDUCE_STATE_FROM_SOLR', data: solr });
-		}
-		dispatch({ type: 'SET_SOLR_EDITABLE', data: isSolrEditable });
-	}, [isSolrEditable]);
 
 	return (
 		<div className={`synonym-solr-editor metabox-holder ${!isSolrVisible ? 'hidden' : ''}`}>
@@ -38,9 +26,11 @@ const SolrEditor = () => {
 						id="ep-synonym-input"
 						name={synonymsTextareaInputName}
 						rows="20"
-						value={solr}
+						value={reducedState}
 						readOnly={!isSolrEditable}
-						onChange={(e) => setSolr(e.target.value)}
+						onChange={(event) =>
+							dispatch({ type: 'REDUCE_STATE_FROM_SOLR', data: event.target.value })
+						}
 					/>
 				</div>
 			</div>
