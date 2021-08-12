d94bd1b4ca8899670facad489283f90c1c76a018
Ricardo Moraleida <ricardo.moraleida@10up.com>
2021-08-11 23:17:11 -0300

    add filter docblocks, move logic around replace search text tokens

--- includes/classes/Feature/BooleanSearchOperators/BooleanSearchOperators.php
+++ includes/classes/Feature/BooleanSearchOperators/BooleanSearchOperators.php
@@ -62,7 +62,7 @@ class BooleanSearchOperators extends Feature {
 	 *
 	 * @hook   ep_elasticpress_enabled
 	 *
-	 * @param  {bool} $enabled Whether to integrate with Elasticsearch or not
+	 * @param  {bool}     $enabled Whether to integrate with Elasticsearch or not
 	 * @param  {WP_Query} $query WP_Query to evaluate
 	 *
 	 * @return bool
@@ -93,22 +93,132 @@ class BooleanSearchOperators extends Feature {
 	 *
 	 * @return array
 	 */
-	public function replace_query_if_boolean( $query, $args, $search_text, $search_fields ) {
+	public function replace_query_if_boolean( $query, $query_vars, $search_text, $search_fields ) {
 
 		if ( $this->query_uses_boolean_operators( $search_text ) ) {
-			$search_text = \str_replace( array( ' AND ', ' OR ', ' NOT ' ), array( ' +', ' | ', ' -' ), $search_text );
 
-			$simple_query_string = array(
+			$simple_query = array(
 				'simple_query_string' => array(
-					'query'                               => $search_text,
-					'fields'                              => \apply_filters( 'ep_boolean_operators_fields', $search_fields ),
-					'default_operator'                    => \apply_filters( 'ep_boolean_operators_default', 'OR' ),
-					'flags'                               => \apply_filters( 'ep_boolean_operators_flags', 'ALL' ),
-					'auto_generate_synonyms_phrase_query' => \apply_filters( 'ep_boolean_operators_generate_synonyms', true ),
+
+					/**
+					 * Filter the fields to use in boolean operator searches
+					 *
+					 * @hook    ep_boolean_operators_fields
+					 *
+					 * @param   {array} $search_fields
+					 * @param   {array}  $query_vars    Query variables
+					 * @param   {string} $search_text   Search text modified to replace tokens
+					 * @param   {array}  $search_fields Search fields
+					 * @param   {array}  $query         The original query
+					 *
+					 * @return  {array} New fields
+					 */
+					'fields'                              => \apply_filters( 'ep_boolean_operators_fields', $search_fields, $query_vars, $search_text, $query ),
+
+					/**
+					 * Filter the default boolean operator
+					 * Valid values: OR, AND
+					 *
+					 * @hook    ep_boolean_operators_default
+					 *
+					 * @param   {string} $default
+					 * @param   {array}  $query_vars    Query variables
+					 * @param   {string} $search_text   Search text modified to replace tokens
+					 * @param   {array}  $search_fields Search fields
+					 * @param   {array}  $query         The original query
+					 *
+					 * @return  {string} New operator
+					 */
+					'default_operator'                    => \apply_filters( 'ep_boolean_operators_default', 'OR', $query_vars, $search_text, $search_fields, $query ),
+
+					/**
+					 * Filter allowed boolean operators.
+					 * Valid flags: ALL, AND, ESCAPE, FUZZY, NEAR, NONE, NOT, OR, PHRASE, PRECEDENCE, PREFIX, SLOP, WHITESPACE
+					 * Must return a string with a single flag or use pipe separators, e.g.: 'OR|AND|PREFIX'
+					 *
+					 * @hook    ep_boolean_operators_flags
+					 *
+					 * @param   {string} $flags
+					 * @param   {array}  $query_vars    Query variables
+					 * @param   {string} $search_text   Search text modified to replace tokens
+					 * @param   {array}  $search_fields Search fields
+					 * @param   {array}  $query         The original query
+					 *
+					 * @return  {string} New flags
+					 */
+					'flags'                               => \apply_filters( 'ep_boolean_operators_flags', 'ALL', $query_vars, $search_text, $search_fields, $query ),
+
+					/**
+					 * Filter automatic synonym generation for boolean operators queries
+					 *
+					 * @hook    ep_boolean_operators_generate_synonyms
+					 *
+					 * @param   {bool} $auto_generate_synonyms
+					 * @param   {array}  $query_vars    Query variables
+					 * @param   {string} $search_text   Search text modified to replace tokens
+					 * @param   {array}  $search_fields Search fields
+					 * @param   {array}  $query         The original query
+					 *
+					 * @return  {bool} New fuzziness
+					 */
+					'auto_generate_synonyms_phrase_query' => \apply_filters( 'ep_boolean_operators_generate_synonyms', true, $query_vars, $search_text, $search_fields, $query ),
 				),
 			);
 
-			return \apply_filters( 'ep_boolean_operators_query_args', $simple_query_string, $args, $search_text, $search_fields, $query );
+			$original_text = $search_text;
+
+			if ( 'ALL' === $simple_query['simple_query_string']['flags'] ) {
+				$search_text = \str_replace( array( ' AND ', ' OR ', ' NOT ' ), array( ' +', ' | ', ' -' ), $search_text );
+			} else {
+				$flags = explode( '|', $simple_query['simple_query_string']['flags'] );
+				$ops = array( 'AND', 'OR', 'NOT' );
+
+				foreach( $ops as $flag ) {
+					if ( \in_array( $flag, $flags, true ) ) {
+						switch ( $flag ) {
+							case 'AND':
+								$search_text = \str_replace( " $flag ", ' +', $search_text );
+								break;
+							case 'OR':
+								$search_text = \str_replace( " $flag ", ' | ', $search_text );
+								break;
+							case 'NOT':
+								$search_text = \str_replace( " $flag ", ' -', $search_text );
+								break;
+						}
+					}
+				}
+			}
+
+			/**
+			 * Filter the search text to use in boolean operator queries
+			 *
+			 * @hook    ep_boolean_operators_search_text
+			 *
+			 * @param   {string} $search_text   Search text modified to replace tokens
+			 * @param   {string} $original_text The original search text
+			 * @param   {array}  $query_vars    Query variables
+			 * @param   {array}  $search_fields Search fields
+			 * @param   {array}  $query         The original query
+			 *
+			 * @return  {string} New search text
+			 */
+			$simple_query['simple_query_string']['query'] = \apply_filters( 'ep_boolean_operators_search_text', $search_text, $original_text, $query_vars, $search_fields, $query );
+
+			/**
+			 * Filter formatted Elasticsearch simple query string query (only contains query part)
+			 *
+			 * @hook   ep_boolean_operators_query_args
+			 *
+			 * @param  {array}  $simple_query  Current query
+			 * @param  {array}  $query_vars    Query variables
+			 * @param  {string} $search_text   Search text modified to replace tokens
+			 * @param  {array}  $search_fields Search fields
+			 * @param  {array}  $query         The original query
+			 *
+			 * @return {array} New query
+			 */
+			return \apply_filters( 'ep_boolean_operators_query_args', $simple_query, $query_vars, $search_text, $search_fields, $query );
 		}
 
 		return $query;
@@ -193,13 +303,16 @@ class BooleanSearchOperators extends Feature {
 		<p><?php esc_html_e( 'Allows users to search using the following boolean operators:', 'elasticpress' ); ?></p>
 		<ul>
 			<li>
-				<code>+</code> or <code>AND</code> <?php esc_html_e( 'signifies AND operation. eg.: modern +art, modern AND art', 'elasticpress' ); ?>
+				<code>+</code> or
+				<code>AND</code> <?php esc_html_e( 'signifies AND operation. eg.: modern +art, modern AND art', 'elasticpress' ); ?>
 			</li>
 			<li>
-				<code>|</code> or <code>OR</code> <?php esc_html_e( 'signifies OR operation. eg.: modern | art, modern OR art', 'elasticpress' ); ?>
+				<code>|</code> or
+				<code>OR</code> <?php esc_html_e( 'signifies OR operation. eg.: modern | art, modern OR art', 'elasticpress' ); ?>
 			</li>
 			<li>
-				<code>-</code> or <code>NOT</code> <?php esc_html_e( 'signifies NOT operation. eg.: modern -art, modern NOT art', 'elasticpress' ); ?>
+				<code>-</code> or
+				<code>NOT</code> <?php esc_html_e( 'signifies NOT operation. eg.: modern -art, modern NOT art', 'elasticpress' ); ?>
 			</li>
 			<li>
 				<code>"</code> <?php esc_html_e( 'wraps characters to signify a phrase. eg.: "modern art"', 'elasticpress' ); ?>
