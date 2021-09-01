9f914d59a941052c8a3773d69b7a7b1f5479cc71
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2021-08-31 21:06:27 -0600

    CLI: Explicitly state to offset only when no advanced pagination

--- includes/classes/Command.php
+++ includes/classes/Command.php
@@ -1018,7 +1018,9 @@ class Command extends WP_CLI_Command {
 					$peak_memory    = ' (Peak: ' . round( memory_get_peak_usage() / 1024 / 1024, 2 ) . 'mb)';
 					WP_CLI::log( WP_CLI::colorize( '%Y' . esc_html__( 'Memory Usage: ', 'elasticpress' ) . '%N' . $current_memory . $peak_memory ) );
 				}
-			} else {
+			}
+
+			if ( ! $query_args['ep_indexing_advanced_pagination'] ) {
 				// Only increment the offset if not using advanced pagination.
 				// For the advanced pagination should always be 0.
 				// @see Indexable\Post\Post.php::query_db.
