2a87ddeba33187dfd774e353e17971e1753b098d
Oscar Sanchez <oscar.sanchez@10up.com>
2021-09-13 19:07:13 -0500

    Fix linting issues

--- includes/classes/Indexable/Post/Post.php
+++ includes/classes/Indexable/Post/Post.php
@@ -1005,11 +1005,11 @@ class Post extends Indexable {
 		foreach ( $taxonomies as $tax_slug => $tax ) {
 			// Prevents duplication of core's default taxonomies post_tag and category in ES query.
 			apply_filters(
-					'ep_post_tax_excluded_wp_query_root_check',
-					$excluded_tax_from_root_check = [
+				'ep_post_tax_excluded_wp_query_root_check',
+				$excluded_tax_from_root_check = [
 					'category',
 					'post_tag',
-					]
+				]
 			);
 
 			if ( $tax->query_var && ! empty( $args[ $tax->query_var ] ) && ! in_array( $tax->name, $excluded_tax_from_root_check, true ) ) {
