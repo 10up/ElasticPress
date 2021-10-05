cf55d699388d6f52efdac1984708872993829ceb
Ramon <Rahmon@users.noreply.github.com>
2021-10-05 09:06:42 -0300

    Check if query is using elasticsearch on testProductsPostTypeQueryProductCatTax

--- tests/php/features/TestWooCommerce.php
+++ tests/php/features/TestWooCommerce.php
@@ -115,7 +115,7 @@ class TestWooCommerce extends BaseTestCase {
 
 		$query = new \WP_Query( $args );
 
-		$this->assertTrue( ! empty( $this->fired_actions['ep_wp_query_search'] ) );
+		$this->assertTrue( $query->elasticsearch_success );
 	}
 
 	/**
