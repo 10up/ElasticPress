4c8eab23e2a9698eb90c72488ee224b48e243313
Ramon <Rahmon@users.noreply.github.com>
2021-10-10 18:43:07 -0300

    Check if Elasticsearch was used in testProductsPostTypeQueryOn

--- tests/php/features/TestWooCommerce.php
+++ tests/php/features/TestWooCommerce.php
@@ -80,6 +80,8 @@ class TestWooCommerce extends BaseTestCase {
 		);
 
 		$query = new \WP_Query( $args );
+
+		$this->assertTrue( $query->elasticsearch_success );
 		$this->assertEquals( 1, $query->post_count );
 		$this->assertEquals( 1, $query->found_posts );
 
