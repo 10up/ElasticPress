6abd0c227b0f6a8c00d6d877df1bae7389123bba
Ramon <Rahmon@users.noreply.github.com>
2021-11-08 20:00:14 -0300

    Remove pause button from header

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -23,7 +23,7 @@ $is_sync_page = 'sync' === Screen::factory()->get_current_screen();
 	<div class="icons">
 		<span class="sync-status"></span>
 		<?php if ( $is_sync_page ) : ?>
-			<a class="dashicons pause-sync dashicons-controls-pause" title="<?php esc_attr_e( 'Pause Sync', 'elasticpress' ); ?>" aria-label="<?php esc_attr_e( 'Pause Sync', 'elasticpress' ); ?>"></a>
+
 			<a class="dashicons resume-sync dashicons-controls-play" title ="<?php esc_attr_e( 'Resume Sync', 'elasticpress' ); ?>" aria-label="<?php esc_attr_e( 'Resume Sync', 'elasticpress' ); ?>"></a>
 			<a class="dashicons cancel-sync dashicons-no" title="<?php esc_attr_e( 'Cancel Sync', 'elasticpress' ); ?>" aria-label="<?php esc_attr_e( 'Cancel Sync', 'elasticpress' ); ?>"></a>
 		<?php endif; ?>
