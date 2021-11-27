3e1f18d4b1314560f893e19ee958d33b8ac4e927
Felipe Elia <felipe.elia@gmail.com>
2021-11-27 13:56:38 -0300

    Lint + properly escaped wpCli + updateFeatures

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -24,68 +24,62 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add(
-	'login',
-	( username = 'admin', password = 'password' ) => {
-		cy.visit( `/wp-admin` );
-		cy.get( 'body' ).then( ( $body ) => {
-			if ( $body.find( '#wpwrap' ).length == 0 ) {
-				cy.get( 'input#user_login' ).clear();
-				cy.get( 'input#user_login' ).click().type( username );
-				cy.get( 'input#user_pass' ).type( `${ password }{enter}` );
-			}
-		} );
-	}
-);
+Cypress.Commands.add('login', (username = 'admin', password = 'password') => {
+	cy.visit(`/wp-admin`);
+	cy.get('body').then(($body) => {
+		if ($body.find('#wpwrap').length == 0) {
+			cy.get('input#user_login').clear();
+			cy.get('input#user_login').click().type(username);
+			cy.get('input#user_pass').type(`${password}{enter}`);
+		}
+	});
+});
 
-Cypress.Commands.add( 'visitAdminPage', ( page = 'index.php' ) => {
+Cypress.Commands.add('visitAdminPage', (page = 'index.php') => {
 	cy.login();
-	if ( page.includes( 'http' ) ) {
-		cy.visit( page );
+	if (page.includes('http')) {
+		cy.visit(page);
 	} else {
-		cy.visit( `/wp-admin/${ page.replace( /^\/|\/$/g, '' ) }` );
+		cy.visit(`/wp-admin/${page.replace(/^\/|\/$/g, '')}`);
 	}
-} );
+});
 
-Cypress.Commands.add(
-	'createTaxonomy',
-	( name = 'Test taxonomy', taxonomy = 'category' ) => {
-		cy.visitAdminPage( `edit-tags.php?taxonomy=${ taxonomy }` );
-		cy.get( '#tag-name' ).click().type( `${ name }{enter}` );
-	}
-);
+Cypress.Commands.add('createTaxonomy', (name = 'Test taxonomy', taxonomy = 'category') => {
+	cy.visitAdminPage(`edit-tags.php?taxonomy=${taxonomy}`);
+	cy.get('#tag-name').click().type(`${name}{enter}`);
+});
 
-Cypress.Commands.add( 'openDocumentSettingsSidebar', () => {
+Cypress.Commands.add('openDocumentSettingsSidebar', () => {
 	const button =
 		'.edit-post-header__settings button[aria-label="Settings"][aria-expanded="false"]';
-	cy.get( 'body' ).then( ( $body ) => {
-		if ( $body.find( button ).length > 0 ) {
-			cy.get( button ).click();
+	cy.get('body').then(($body) => {
+		if ($body.find(button).length > 0) {
+			cy.get(button).click();
 		}
-	} );
-	cy.get( '.edit-post-sidebar__panel-tab' ).contains( 'Post' ).click();
-} );
+	});
+	cy.get('.edit-post-sidebar__panel-tab').contains('Post').click();
+});
 
-Cypress.Commands.add( 'openDocumentSettingsPanel', ( name ) => {
+Cypress.Commands.add('openDocumentSettingsPanel', (name) => {
 	cy.openDocumentSettingsSidebar();
-	cy.get( '.components-panel__body .components-panel__body-title button' )
-		.contains( name )
-		.then( ( panel ) => {
-			if ( ! panel.hasClass( '.is-opened' ) ) {
-				cy.get( panel ).click();
-				cy.get( panel )
-					.parents( '.components-panel__body' )
-					.should( 'have.class', 'is-opened' );
+	cy.get('.components-panel__body .components-panel__body-title button')
+		.contains(name)
+		.then((panel) => {
+			if (!panel.hasClass('.is-opened')) {
+				cy.get(panel).click();
+				cy.get(panel).parents('.components-panel__body').should('have.class', 'is-opened');
 			}
-		} );
-} );
+		});
+});
 
-Cypress.Commands.add( 'clearThenType', { prevSubject: true }, (subject, text) => {
-    cy.wrap( subject ).clear().type( text );
-  }
-);
+Cypress.Commands.add('clearThenType', { prevSubject: true }, (subject, text) => {
+	cy.wrap(subject).clear().type(text);
+});
 
-Cypress.Commands.add( 'wpCli', ( command ) => {
-	const escapedCommand = command.replace(/"/g, '\\"').replace( /^wp /, '');
-	cy.exec( `npm run env run tests-cli ${escapedCommand}` )
-} );
+Cypress.Commands.add('wpCli', (command) => {
+	const escapedCommand = command.replace(/"/g, '\\"').replace(/^wp /, '');
+	cy.exec(`npm run env run tests-cli "${escapedCommand}"`).then((result) => {
+		result.stdout = result.stdout.split('\n').slice(3).join('\n');
+		cy.wrap(result);
+	});
+});
--- tests/cypress/support/global-hooks.js
+++ tests/cypress/support/global-hooks.js
@@ -1,36 +1,51 @@
-before( () =>  {
-
+before(() => {
 	const features = {
-		'search': {
-			'active'           :  1,
-			'highlight_enabled':  true,
-			'highlight_excerpt':  true,
-			'highlight_tag'    :  'mark',
-			'highlight_color'  :  '#157d84',
+		search: {
+			active: 1,
+			highlight_enabled: true,
+			highlight_excerpt: true,
+			highlight_tag: 'mark',
+			highlight_color: '#157d84',
 		},
-		'related_posts'     : {
-			'active':  1,
+		related_posts: {
+			active: 1,
 		},
-		'facets'            : {
-			'active':  1,
+		facets: {
+			active: 1,
 		},
-		'searchordering'    : {
-			'active':  1,
+		searchordering: {
+			active: 1,
 		},
-		'autosuggest'       : {
-			'active':  1,
+		autosuggest: {
+			active: 1,
 		},
-		'woocommerce'       : {
-			'active':  0,
+		woocommerce: {
+			active: 0,
 		},
-		'protected_content' : {
-			'active':  0,
+		protected_content: {
+			active: 0,
 		},
-		'users'             : {
-			'active':  1,
+		users: {
+			active: 1,
 		},
-	}
+	};
 
-	const escapedFeatures = JSON.stringify( features ).replace(/"/g, '\"').replace(/'/g, '\'');
-	cy.exec( `npm run env run tests-cli "wp eval \\"update_option( 'ep_feature_settings', '${escapedFeatures}' );\\""` );
-} );
+	const updateFeatures = (features) => {
+		const escapedFeatures = JSON.stringify(features).replace(/"/g, '"').replace(/'/g, "'");
+		cy.wpCli(`eval "update_option( 'ep_feature_settings', '${escapedFeatures}' );"`);
+	};
+
+	cy.wpCli('eval "echo ElasticPress\\Utils\\get_host();"').then((epHost) => {
+		// Nothing needs to be done if EP.io.
+		if (!epHost.stdout.match(/elasticpress\.io/)) {
+			cy.wpCli(
+				'eval "echo \\ElasticPress\\Indexables::factory()->get( \'post\' )->get_index_name();"',
+			).then((indexName) => {
+				features.autosuggest.endpoint_url = `${epHost.stdout}/${indexName.stdout}/_search`;
+				updateFeatures(features);
+			});
+		} else {
+			updateFeatures(features);
+		}
+	});
+});
