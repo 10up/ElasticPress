41946d646b2ae506bed90e38f60468cedd30edb8
Felipe Elia <felipe.elia@gmail.com>
2021-12-23 13:34:00 -0300

    Tweak get_docker_cid to use only one ID

--- tests/cypress/wordpress-files/test-mu-plugins/unique-index-name.php
+++ tests/cypress/wordpress-files/test-mu-plugins/unique-index-name.php
@@ -17,8 +17,21 @@ add_action( 'admin_footer', function() {
 	);
 });
 
+/**
+ * Return a unique id based on the docker environment.
+ *
+ * As in `wp-env` a container is used for cli and another one for web server accesses,
+ * we pick the first one called, store it, and use it across all calls.
+ *
+ * @return string
+ */
 function get_docker_cid() {
-	return exec( 'cat /etc/hostname' );
+	$docker_cid = get_option( 'ep_tests_docker_cid', '' );
+	if ( ! $docker_cid ) {
+		$docker_cid = exec( 'cat /etc/hostname' );
+		update_option( 'ep_tests_docker_cid', $docker_cid );
+	}
+	return $docker_cid;
 }
 
 add_filter( 'ep_es_info_cache_expiration', '__return_zero' );
