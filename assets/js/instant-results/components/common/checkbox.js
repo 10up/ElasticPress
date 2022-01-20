8e43202c6092efee8d00e4ce4b43b1c1b89e750f
Jacob Peattie <jacob.peattie@10up.com>
2022-01-20 22:13:32 +1100

    Display result counts for facet constraints. Closes #2526.

--- assets/js/instant-results/components/common/checkbox.js
+++ assets/js/instant-results/components/common/checkbox.js
@@ -7,17 +7,18 @@ import { WPElement } from '@wordpress/element';
  * Checkbox component.
  *
  * @param {Option} props Component props.
+ * @param {string} props.count Checkbox count.
  * @param {string} props.id Checkbox ID.
  * @param {string} props.label Checkbox label.
  *
  * @return {WPElement} Component element.
  */
-export default ({ id, label, ...props }) => {
+export default ({ count, id, label, ...props }) => {
 	return (
 		<div className="ep-search-checkbox">
-			<input className="ep-search-checkbox__input" id={id} type="checkbox" {...props} />
+			<input className="ep-search-checkbox__input" id={id} type="checkbox" {...props} />{' '}
 			<label className="ep-search-checkbox__label" htmlFor={id}>
-				{label}
+				{label} {count && <span className="ep-search-checkbox__count">{count}</span>}
 			</label>
 		</div>
 	);
