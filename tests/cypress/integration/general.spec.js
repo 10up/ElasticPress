8e57edd385af25f2be7a979e6f96e361130081b0
Felipe Elia <felipe.elia@gmail.com>
2022-02-10 16:23:43 -0300

    Start of the general tests

--- tests/cypress/integration/general.spec.js
+++ tests/cypress/integration/general.spec.js
@@ -1,1 +1,9 @@
-describe('WordPress can perform standard ElasticPress actions', () => {});
+describe('WordPress can perform standard ElasticPress actions', () => {
+	it('Can see the settings page link in WordPress Dashboard', () => {
+		cy.login();
+
+		cy.activatePlugin('elasticpress', 'dashboard', true);
+
+		cy.get('.toplevel_page_elasticpress .wp-menu-name').should('contain.text', 'ElasticPress');
+	});
+});
