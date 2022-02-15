bb9bf1610cff2d4fcbccbfae7f21dd98ad452a4a
Felipe Elia <felipe.elia@gmail.com>
2022-02-15 16:28:30 -0300

    Activate the plugin only in the current site

--- tests/cypress/integration/general.spec.js
+++ tests/cypress/integration/general.spec.js
@@ -2,7 +2,7 @@ describe('WordPress can perform standard ElasticPress actions', () => {
 	it('Can see the settings page link in WordPress Dashboard', () => {
 		cy.login();
 
-		cy.activatePlugin('elasticpress', 'dashboard', true);
+		cy.activatePlugin('elasticpress', 'dashboard');
 
 		cy.get('.toplevel_page_elasticpress .wp-menu-name').should('contain.text', 'ElasticPress');
 	});
