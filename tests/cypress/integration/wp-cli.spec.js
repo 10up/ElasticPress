6e422a90bee3f4869b5f10a7b52577a4f3ac6131
Felipe Elia <felipe.elia@gmail.com>
2022-02-16 12:53:28 -0300

    Other WP-CLI tests

--- tests/cypress/integration/wp-cli.spec.js
+++ tests/cypress/integration/wp-cli.spec.js
@@ -27,46 +27,47 @@ describe('WP-CLI Commands', () => {
 
 	context('wp elasticpress index', () => {
 		it('Can index all the posts of the current blog', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress index').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Indexing posts');
-				expect(wpCliResponse.stdout).to.contains('Number of posts indexed');
-			});
+			cy.wpCli('wp elasticpress index')
+				.its('stdout')
+				.should('contain', 'Indexing posts')
+				.should('contain', 'Number of posts indexed');
 
 			checkIfNotMissingIndexes();
 		});
 
 		it('Can clear the index in Elasticsearch, put the mapping again and then index all the posts if user specifies --setup argument', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress index --setup --yes').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Mapping sent');
-				expect(wpCliResponse.stdout).to.contains('Indexing posts');
-				expect(wpCliResponse.stdout).to.contains('Number of posts indexed');
-			});
+			cy.wpCli('wp elasticpress index --setup --yes')
+				.its('stdout')
+				.should('contain', 'Mapping sent')
+				.should('contain', 'Indexing posts')
+				.should('contain', 'Number of posts indexed');
+
+			cy.wpCli('wp elasticpress stats')
+				.its('stdout')
+				.should('contain', 'Documents')
+				.should('contain', 'Index Size');
 
 			checkIfNotMissingIndexes();
 		});
 
 		it('Can process that many posts in bulk index per round if user specifies --per-page parameter', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress index --per-page=20').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Indexing posts');
-				expect(wpCliResponse.stdout).to.contains('Processed 20/');
-				expect(wpCliResponse.stdout).to.contains('Processed 40/');
-				expect(wpCliResponse.stdout).to.contains('Number of posts indexed');
-			});
+			cy.wpCli('wp elasticpress index --per-page=20')
+				.its('stdout')
+				.should('contain', 'Indexing posts')
+				.should('contain', 'Processed 20/')
+				.should('contain', 'Processed 40/')
+				.should('contain', 'Number of posts indexed');
 		});
 
 		it('Can index one post at a time if user specifies --nobulk parameter', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress index --nobulk').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Indexing posts');
-				expect(wpCliResponse.stdout).to.contains('Processed 1/');
-				expect(wpCliResponse.stdout).to.contains('Processed 2/');
-				expect(wpCliResponse.stdout).to.contains('Processed 3/');
-				expect(wpCliResponse.stdout).to.contains('Processed 4/');
-				expect(wpCliResponse.stdout).to.contains('Number of posts indexed');
-			});
+			cy.wpCli('wp elasticpress index --nobulk')
+				.its('stdout')
+				.should('contain', 'Indexing posts')
+				.should('contain', 'Processed 1/')
+				.should('contain', 'Processed 2/')
+				.should('contain', 'Processed 3/')
+				.should('contain', 'Processed 4/')
+				.should('contain', 'Number of posts indexed');
 		});
 
 		it('Can skip X posts and index the remaining if user specifies --offset parameter', () => {
@@ -114,37 +115,82 @@ describe('WP-CLI Commands', () => {
 	});
 
 	it('Can delete the index of current blog if user runs wp elasticpress delete-index', () => {
-		// eslint-disable-next-line jest/valid-expect-in-promise
-		cy.wpCli('wp elasticpress delete-index --yes').then((wpCliResponse) => {
-			expect(wpCliResponse.stdout).to.contains('Index deleted');
+		cy.wpCli('wp elasticpress delete-index --yes')
+			.its('stdout')
+			.should('contain', 'Index deleted');
 
-			cy.login();
+		cy.wpCli('wp elasticpress stats', true)
+			.its('stderr')
+			.should('contain', 'is not currently indexed');
 
-			cy.visitAdminPage('admin.php?page=elasticpress-health');
-			cy.get('.wrap').should(
-				'contain.text',
-				'We could not find any data for your Elasticsearch indices.',
-			);
-		});
+		cy.login();
+
+		cy.visitAdminPage('admin.php?page=elasticpress-health');
+		cy.get('.wrap').should(
+			'contain.text',
+			'We could not find any data for your Elasticsearch indices.',
+		);
 	});
 
-	it('Can put mapping of the current blog if user runs wp elasticpress put-mapping', () => {});
+	it('Can put mapping of the current blog if user runs wp elasticpress put-mapping', () => {
+		cy.wpCli('wp elasticpress put-mapping')
+			.its('stdout')
+			.should('contain', 'Adding post mapping')
+			.should('contain', 'Mapping sent');
+	});
 
 	it('Can recreate the alias index which points to every index in the network if user runs wp elasticpress recreate-network-alias command', () => {});
 
-	it('Can activate a feature if user runs wp elasticpress activate-feature and specify feature', () => {});
+	it('Can activate and deactivate a feature', () => {
+		cy.wpCli('wp elasticpress activate-feature search', true)
+			.its('stderr')
+			.should('contain', 'This feature is already active');
 
-	it('Can deactivate a feature if user runs wp elasticpress deactivate-feature and specify feature', () => {});
+		cy.wpCli('wp elasticpress deactivate-feature search')
+			.its('stdout')
+			.should('contain', 'Feature deactivated');
 
-	it('Can list all the active features if user runs wp elasticpress list-features command', () => {});
+		cy.wpCli('wp elasticpress deactivate-feature search', true)
+			.its('stderr')
+			.should('contain', 'Feature is not active');
 
-	it('Can list all the registered features if user runs wp elasticpress list-features --all command', () => {});
+		cy.wpCli('wp elasticpress activate-feature search')
+			.its('stdout')
+			.should('contain', 'Feature activated');
+	});
 
-	it('Can return the number of documents indexed and index size if user runs wp elasticpress stats command', () => {});
+	it('Can list all the active features if user runs wp elasticpress list-features command', () => {
+		cy.wpCli('wp elasticpress list-features')
+			.its('stdout')
+			.should('contain', 'Active features');
+	});
 
-	it('Can return a string indicating the index is not running', () => {});
+	it('Can list all the registered features if user runs wp elasticpress list-features --all command', () => {
+		cy.wpCli('wp elasticpress list-features --all')
+			.its('stdout')
+			.should('contain', 'Registered features');
+	});
 
-	it('Can return a string indicating with the appropriate fields if user runs wp elasticpress get-last-cli-index command', () => {});
+	it('Can return a string indicating the index is not running', () => {
+		cy.wpCli('wp elasticpress get-indexing-status')
+			.its('stdout')
+			.should(
+				'contain',
+				'{"indexing":false,"method":"none","items_indexed":0,"total_items":-1}',
+			);
+	});
+
+	it('Can return a string indicating with the appropriate fields if user runs wp elasticpress get-last-cli-index command', () => {
+		cy.wpCli('wp elasticpress index');
+
+		cy.wpCli('wp elasticpress get-last-cli-index --clear')
+			.its('stdout')
+			.should('contain', '"total_time"');
+
+		cy.wpCli('wp elasticpress get-last-cli-index --clear')
+			.its('stdout')
+			.should('contain', '[]');
+	});
 
 	context('multisite parameters', () => {
 		before(() => {
@@ -161,29 +207,32 @@ describe('WP-CLI Commands', () => {
 		});
 
 		it('Can index all blogs in network if user specifies --network-wide argument', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress index --network-wide').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Indexing posts on site');
-				expect(wpCliResponse.stdout).to.contains('Number of posts indexed on site');
-			});
+			cy.wpCli('wp elasticpress index --network-wide')
+				.its('stdout')
+				.should('contain', 'Indexing posts on site')
+				.should('contain', 'Number of posts indexed on site');
 
 			checkIfNotMissingIndexes('network');
 		});
 
-		it('Can delete all the index network-wide if user runs wp elasticpress delete-index --network-wide', () => {
-			// eslint-disable-next-line jest/valid-expect-in-promise
-			cy.wpCli('wp elasticpress delete-index --network-wide --yes').then((wpCliResponse) => {
-				expect(wpCliResponse.stdout).to.contains('Deleting post index for site');
-				expect(wpCliResponse.stdout).to.contains('Index deleted');
-			});
+		it('Can delete all the indices and put mappings for the entire network-wide', () => {
+			cy.wpCli('wp elasticpress delete-index --network-wide --yes')
+				.its('stdout')
+				.should('contain', 'Deleting post index for site')
+				.should('contain', 'Index deleted');
 
 			cy.visitAdminPage('network/admin.php?page=elasticpress-health');
 			cy.get('.wrap').should(
 				'contain.text',
 				'We could not find any data for your Elasticsearch indices.',
 			);
-		});
 
-		it('Can put mapping network-wide if user runs wp elasticpress put-mapping --network-wide', () => {});
+			cy.wpCli('wp elasticpress put-mapping --network-wide')
+				.its('stdout')
+				.should('contain', 'Adding post mapping for site')
+				.should('contain', 'Mapping sent');
+
+			checkIfNotMissingIndexes('network');
+		});
 	});
 });
