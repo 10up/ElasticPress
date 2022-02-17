d039cbb4255579e3b5c76a84ea18ab9be68ef2fa
Felipe Elia <felipe.elia@gmail.com>
2022-02-17 15:00:44 -0300

    Clear previous stuck syncs

--- tests/cypress/support/global-hooks.js
+++ tests/cypress/support/global-hooks.js
@@ -1,4 +1,9 @@
 before(() => {
+	// Clear sync from previous tests.
+	cy.wpCli(
+		'wp eval \'delete_transient( "ep_wpcli_sync" ); delete_option( "ep_index_meta" ); delete_site_transient( "ep_wpcli_sync" ); delete_site_option( "ep_index_meta" );\'',
+	);
+
 	cy.wpCli('wp elasticpress get-indexes').then((wpCliResponse) => {
 		window.indexNames = JSON.parse(wpCliResponse.stdout);
 	});
