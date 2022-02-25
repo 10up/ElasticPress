6b7a407bec7a7bcbeaa9e5816c53302c7f985c8f
Felipe Elia <felipe.elia@gmail.com>
2022-02-25 14:20:27 -0300

    Fix url being set wrong after multisite convert

--- bin/setup-cypress-env.sh
+++ bin/setup-cypress-env.sh
@@ -60,6 +60,11 @@ if [ ! -z $EP_INDEX_PREFIX ]; then
 fi
 
 npm run env run tests-cli "wp core multisite-convert"
+
+# Not sure why, wp-env makes it http://localhost:8889/:8889
+npm run env run tests-cli "option set home 'http://localhost:8889'"
+npm run env run tests-cli "option set siteurl 'http://localhost:8889'"
+
 npm run env run tests-cli "wp user create wpsnapshots wpsnapshots@example.test --role=administrator --user_pass=password"
 npm run env run tests-cli "wp super-admin add wpsnapshots"
 
