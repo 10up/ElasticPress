c1ce28bcb3a0e1b071ed490d6a60890e7d6b9bb7
Felipe Elia <felipe.elia@gmail.com>
2022-03-07 09:15:51 -0300

    Revert back user feature activation

--- tests/cypress/integration/indexables/user.spec.js
+++ tests/cypress/integration/indexables/user.spec.js
@@ -37,7 +37,9 @@ describe('User Indexable', () => {
 		cy.maybeDisableFeature('users');
 
 		cy.visitAdminPage('admin.php?page=elasticpress');
-		cy.get('.ep-feature-users form').submit();
+		cy.get('.ep-feature-users .settings-button').click();
+		cy.get('.ep-feature-users [name="settings[active]"][value="1"]').click();
+		cy.get('.ep-feature-users .button-primary').click();
 		cy.on('window:confirm', () => {
 			return true;
 		});
