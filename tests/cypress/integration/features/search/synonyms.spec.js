e486debd0c6603bff8e9a90ecfb6ce6ae4053240
Felipe Elia <felipe.elia@gmail.com>
2022-03-17 15:16:42 -0300

    Synonyms: tests for alternatives

--- tests/cypress/integration/features/search/synonyms.spec.js
+++ tests/cypress/integration/features/search/synonyms.spec.js
@@ -1,4 +1,7 @@
 describe('Post Search Feature - Synonyms Functionality', () => {
+	const word1 = 'authenticity';
+	const word2 = 'credibility';
+
 	before(() => {
 		cy.wpCli("wp post list --post_type='ep-synonym' --format=ids", true).then(
 			(wpCliResponse) => {
@@ -20,31 +23,32 @@ describe('Post Search Feature - Synonyms Functionality', () => {
 
 		const postsData = [
 			{
-				title: 'Testing Synonyms - Shoes',
+				title: `Testing Synonyms - ${word1}`,
 			},
 			{
-				title: 'Testing Synonyms - Sneakers',
+				title: `Testing Synonyms - ${word2}`,
 			},
 		];
 		postsData.forEach((postData) => {
 			cy.publishPost(postData);
 		});
 	});
-	it('Can create, search, and delete synonyms sets', () => {
+	beforeEach(() => {
 		cy.login();
-
+	});
+	it('Can create, search, and delete synonyms sets', () => {
 		// Add the set
 		cy.visitAdminPage('admin.php?page=elasticpress-synonyms');
-		cy.get('.synonym-sets-editor .synonym__remove').click();
-		cy.contains('.synonym-sets-editor .button', 'Add Set').as('addset').click();
-		cy.get('.synonym-sets-editor .ep-synonyms__linked-multi-input').type(
-			'sneakers{enter}shoes{enter}',
-		);
+		cy.get('.synonym-sets-editor').within(() => {
+			cy.get('.synonym__remove').click();
+			cy.contains('.button', 'Add Set').as('addset').click();
+			cy.get('.ep-synonyms__linked-multi-input').type(`${word1}{enter}${word2}{enter}`);
+		});
 		cy.get('#synonym-root .button-primary').click();
 
 		// Check if it works
-		cy.visit('/?s=sneakers');
-		cy.contains('.site-content article h2', 'Shoes').should('exist');
+		cy.visit(`/?s=${word2}`);
+		cy.contains('.site-content article h2', word1).should('exist');
 
 		// Remove the set
 		cy.visitAdminPage('admin.php?page=elasticpress-synonyms');
@@ -52,7 +56,33 @@ describe('Post Search Feature - Synonyms Functionality', () => {
 		cy.get('#synonym-root .button-primary').click();
 
 		// Check if it works
-		cy.visit('/?s=sneakers');
-		cy.contains('.site-content article h2', 'Shoes').should('not.exist');
+		cy.visit(`/?s=${word2}`);
+		cy.contains('.site-content article h2', word1).should('not.exist');
+	});
+	it('Can create, search, and delete synonyms alternatives', () => {
+		// Add the set
+		cy.visitAdminPage('admin.php?page=elasticpress-synonyms');
+		cy.get('.synonym-alternatives-editor').within(() => {
+			cy.get('.synonym__remove').click();
+			cy.contains('.button', 'Add Alternative').as('addset').click();
+			cy.get('.ep-synonyms__input').type(word1);
+			cy.get('.ep-synonyms__linked-multi-input').type(`${word2}{enter}`);
+		});
+		cy.get('#synonym-root .button-primary').click();
+
+		// Check if it works
+		cy.visit(`/?s=${word1}`);
+		cy.contains('.site-content article h2', word2).should('exist');
+		cy.visit(`/?s=${word2}`);
+		cy.contains('.site-content article h2', word1).should('not.exist');
+
+		// Remove the set
+		cy.visitAdminPage('admin.php?page=elasticpress-synonyms');
+		cy.get('.synonym-alternatives-editor .synonym__remove').click();
+		cy.get('#synonym-root .button-primary').click();
+
+		// Check if it works
+		cy.visit(`/?s=${word1}`);
+		cy.contains('.site-content article h2', word2).should('not.exist');
 	});
 });
