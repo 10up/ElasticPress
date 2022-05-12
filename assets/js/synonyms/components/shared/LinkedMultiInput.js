fca51b49ca8ed2309400a990c4b469ee58ec80e0
Jacob Peattie <jacob.peattie@10up.com>
2022-05-12 23:01:23 +1000

    Replace third party dependency with core component.

--- assets/js/synonyms/components/shared/LinkedMultiInput.js
+++ assets/js/synonyms/components/shared/LinkedMultiInput.js
@@ -1,13 +1,13 @@
 /**
  * WordPress dependencies.
  */
+import { FormTokenField } from '@wordpress/components';
 import { useContext, WPElement } from '@wordpress/element';
 
 /**
  * Internal dependencies.
  */
 import { Dispatch } from '../../context';
-import MultiInput from './MultiInput';
 
 /**
  * Linked MultiInput
@@ -24,6 +24,25 @@ const LinkedMultiInput = ({ id, synonyms, removeAction, updateAction }) => {
 	const { removeItemText } = window.epSynonyms.i18n;
 
 	/**
+	 * Handle change to tokens.
+	 *
+	 * @param {string[]} value Array of tokens.
+	 */
+	const handleChange = (value) => {
+		const tokens = value.map((v) => {
+			const token = {
+				label: v,
+				value: v,
+				primary: false,
+			};
+
+			return token;
+		});
+
+		dispatch({ type: updateAction, data: { id, tokens } });
+	};
+
+	/**
 	 * Handle clearing the synonym.
 	 */
 	const handleClear = () => {
@@ -32,11 +51,11 @@ const LinkedMultiInput = ({ id, synonyms, removeAction, updateAction }) => {
 
 	return (
 		<>
-			<MultiInput
+			<FormTokenField
 				key={id}
-				className="ep-synonyms__linked-multi-input"
-				tokens={synonyms}
-				setTokens={(tokens) => dispatch({ type: updateAction, data: { id, tokens } })}
+				label={null}
+				onChange={handleChange}
+				value={synonyms.map((s) => s.value)}
 			/>
 			<button className="synonym__remove" type="button" onClick={handleClear}>
 				<span className="dashicons dashicons-dismiss" />
