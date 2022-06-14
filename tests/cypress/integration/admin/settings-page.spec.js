de8fc14d2a2cd101bcb4898b972a56d415515ca2
Burhan Nasir <burhandodhy@gmail.com>
2022-06-14 10:34:03 +0500

    Move tests to general.spec.js

--- tests/cypress/integration/admin/settings-page.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Settings Page', () => {
-	it('Can see a Sync and Settings buttons on Settings Page', () => {
-		cy.visitAdminPage('admin.php?page=elasticpress-settings');
-		cy.get('.dashicons.start-sync').should('have.attr', 'title', 'Sync Page');
-		cy.get('.dashicons.dashicons-admin-generic').should('have.attr', 'title', 'Settings Page');
-	});
-});
