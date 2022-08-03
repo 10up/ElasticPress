b12fb4f601021b4d819d0095e05711f735479d03
Mohammed Razzaq <mohammed@Mohammeds-MacBook-Air.local>
2022-08-04 00:29:06 +0530

    More test cases for instant results

--- tests/cypress/integration/features/instant-results.spec.js
+++ tests/cypress/integration/features/instant-results.spec.js
@@ -1,10 +1,53 @@
 describe('Instant Results Feature', () => {
 	before(() => {
-		cy.activatePlugin('elasticpress-proxy', 'dashboard');
+		cy.wpCli('eval "echo ElasticPress\\Utils\\get_host();"').then((epHost) => {
+			// Nothing needs to be done if EP.io.
+			if (epHost.stdout.match(/elasticpress\.io/)) {
+				return;
+			}
+			cy.activatePlugin('elasticpress-proxy', 'dashboard');
+		});
 	});
 
 	after(() => {
 		cy.deactivatePlugin('elasticpress-proxy', 'dashboard');
+	});
+
+	it("Can't activate the feature If not in ElasticPress.io nor using a custom PHP proxy", () => {
+		cy.login();
+
+		cy.wpCli('eval "echo ElasticPress\\Utils\\get_host();"').then((epHost) => {
+			// Nothing needs to be done if EP.io.
+			if (epHost.stdout.match(/elasticpress\.io/)) {
+				return;
+			}
+			cy.deactivatePlugin('elasticpress-proxy', 'dashboard');
+			cy.visitAdminPage('admin.php?page=elasticpress');
+			cy.get('.ep-feature-instant-results .settings-button').click();
+			cy.get('.requirements-status-notice').should(
+				'contain.text',
+				'To use this feature you need to be an ElasticPress.io customer or implement a custom proxy',
+			);
+			cy.get('.ep-feature-instant-results .input-wrap').should('have.class', 'disabled');
+			cy.activatePlugin('elasticpress-proxy', 'dashboard');
+		});
+	});
+
+	it('Can see a warning if using cusotom proxy', () => {
+		cy.login();
+		cy.wpCli('eval "echo ElasticPress\\Utils\\get_host();"').then((epHost) => {
+			// Nothing needs to be done if EP.io.
+			if (epHost.stdout.match(/elasticpress\.io/)) {
+				return;
+			}
+			cy.visitAdminPage('admin.php?page=elasticpress');
+			cy.get('.ep-feature-instant-results .settings-button').click();
+			cy.get('.requirements-status-notice').should(
+				'contain.text',
+				'You are using a custom proxy. Make sure you implement all security measures needed',
+			);
+			cy.get('.ep-feature-instant-results .input-wrap').should('not.have.class', 'disabled');
+		});
 	});
 
 	it('Can activate the feature and sync automatically', () => {
