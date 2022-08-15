4b4fc216c08c79973603dbe94c58f8e11678ef8a
Felipe Elia <felipe.elia@gmail.com>
2022-08-15 17:17:42 -0300

    Move some tests back

--- tests/php/features/TestFacet.php
+++ tests/php/features/TestFacet.php
@@ -18,10 +18,40 @@ class TestFacets extends BaseTestCase {
 	 *
 	 * @since 3.6.0
 	 * @group facets
-	 * @expectedDeprecated ElasticPress\Feature\Facets\Facets::get_selected
 	 */
 	public function testGetSelected() {
-		Features::factory()->get_registered_feature( 'facets' )->get_selected();
+		$facet_feature = Features::factory()->get_registered_feature( 'facets' );
+
+		parse_str( 'ep_filter_taxonomy=dolor', $_GET );
+		$selected = $facet_feature->get_selected();
+		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
+
+		parse_str( 'ep_filter_taxonomy=dolor,sit', $_GET );
+		$selected = $facet_feature->get_selected();
+		$this->assertSelectedTax( [ 'dolor' => true, 'sit' => true ], 'taxonomy', $selected );
+
+		parse_str( 'ep_filter_taxonomy=dolor,sit&ep_filter_othertax=amet', $_GET );
+		$selected = $facet_feature->get_selected();
+
+		$this->assertIsArray( $selected );
+		$this->assertIsArray( $selected['taxonomies'] );
+		$this->assertCount( 2, $selected['taxonomies'] );
+		$this->assertArrayHasKey( 'taxonomy', $selected['taxonomies'] );
+		$this->assertArrayHasKey( 'othertax', $selected['taxonomies'] );
+		$this->assertSame( [ 'dolor' => true, 'sit' => true ], $selected['taxonomies']['taxonomy']['terms'] );
+		$this->assertSame( [ 'amet' => true ], $selected['taxonomies']['othertax']['terms'] );
+
+		parse_str( 's=searchterm&ep_filter_taxonomy=dolor', $_GET );
+		$selected = $facet_feature->get_selected();
+		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
+		$this->assertArrayHasKey( 's', $selected );
+		$this->assertSame( 'searchterm', $selected['s'] );
+
+		parse_str( 'post_type=posttype&ep_filter_taxonomy=dolor', $_GET );
+		$selected = $facet_feature->get_selected();
+		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
+		$this->assertArrayHasKey( 'post_type', $selected );
+		$this->assertSame( 'posttype', $selected['post_type'] );
 	}
 
 	/**
@@ -42,5 +72,22 @@ class TestFacets extends BaseTestCase {
 			]
 		];
 		Features::factory()->get_registered_feature( 'facets' )->build_query_url( $filters );
+	}
+
+	/**
+	 * Utilitary function for the testGetSelected test.
+	 *
+	 * Private as it is super specific and not likely to be extended.
+	 *
+	 * @param array  $terms    Array of terms as they should be in the $selected array.
+	 * @param string $taxonomy Taxonomy slug
+	 * @param array  $selected Array of selected terms.
+	 */
+	private function assertSelectedTax( $terms, $taxonomy, $selected ) {
+		$this->assertIsArray( $selected );
+		$this->assertIsArray( $selected['taxonomies'] );
+		$this->assertCount( 1, $selected['taxonomies'] );
+		$this->assertArrayHasKey( $taxonomy, $selected['taxonomies'] );
+		$this->assertSame( $terms, $selected['taxonomies'][ $taxonomy ]['terms'] );
 	}
 }
--- tests/php/features/TestFacetTypeTaxonomy.php
+++ tests/php/features/TestFacetTypeTaxonomy.php
@@ -15,48 +15,6 @@ use ElasticPress\Features as Features;
 class TestFacetTypeTaxonomy extends BaseTestCase {
 
 	/**
-	 * Test the `get_selected` method
-	 *
-	 * @since 4.3.0
-	 * @group facets
-	 */
-	public function testGetSelected() {
-		$facet_feature = Features::factory()->get_registered_feature( 'facets' );
-		$facet_type    = $facet_feature->types['taxonomy'];
-
-		parse_str( 'ep_filter_taxonomy=dolor', $_GET );
-		$selected = $facet_type->get_selected();
-		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
-
-		parse_str( 'ep_filter_taxonomy=dolor,sit', $_GET );
-		$selected = $facet_type->get_selected();
-		$this->assertSelectedTax( [ 'dolor' => true, 'sit' => true ], 'taxonomy', $selected );
-
-		parse_str( 'ep_filter_taxonomy=dolor,sit&ep_filter_othertax=amet', $_GET );
-		$selected = $facet_type->get_selected();
-
-		$this->assertIsArray( $selected );
-		$this->assertIsArray( $selected['taxonomies'] );
-		$this->assertCount( 2, $selected['taxonomies'] );
-		$this->assertArrayHasKey( 'taxonomy', $selected['taxonomies'] );
-		$this->assertArrayHasKey( 'othertax', $selected['taxonomies'] );
-		$this->assertSame( [ 'dolor' => true, 'sit' => true ], $selected['taxonomies']['taxonomy']['terms'] );
-		$this->assertSame( [ 'amet' => true ], $selected['taxonomies']['othertax']['terms'] );
-
-		parse_str( 's=searchterm&ep_filter_taxonomy=dolor', $_GET );
-		$selected = $facet_type->get_selected();
-		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
-		$this->assertArrayHasKey( 's', $selected );
-		$this->assertSame( 'searchterm', $selected['s'] );
-
-		parse_str( 'post_type=posttype&ep_filter_taxonomy=dolor', $_GET );
-		$selected = $facet_type->get_selected();
-		$this->assertSelectedTax( [ 'dolor' => true ], 'taxonomy', $selected );
-		$this->assertArrayHasKey( 'post_type', $selected );
-		$this->assertSame( 'posttype', $selected['post_type'] );
-	}
-
-	/**
 	 * Test build query URL
 	 *
 	 * @since 4.3.0
@@ -166,22 +124,5 @@ class TestFacetTypeTaxonomy extends BaseTestCase {
 		];
 		$changed_args = $facet_type->set_agg_filters( $args, $query_args, $query );
 		$this->assertArrayHasKey( 'filter', $changed_args['aggs']['terms'] );
-	}
-
-	/**
-	 * Utilitary function for the testGetSelected test.
-	 *
-	 * Private as it is super specific and not likely to be extended.
-	 *
-	 * @param array  $terms    Array of terms as they should be in the $selected array.
-	 * @param string $taxonomy Taxonomy slug
-	 * @param array  $selected Array of selected terms.
-	 */
-	private function assertSelectedTax( $terms, $taxonomy, $selected ) {
-		$this->assertIsArray( $selected );
-		$this->assertIsArray( $selected['taxonomies'] );
-		$this->assertCount( 1, $selected['taxonomies'] );
-		$this->assertArrayHasKey( $taxonomy, $selected['taxonomies'] );
-		$this->assertSame( $terms, $selected['taxonomies'][ $taxonomy ]['terms'] );
 	}
 }
