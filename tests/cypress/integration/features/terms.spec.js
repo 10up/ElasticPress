7e0998e2feac809712a61a27fe8285892c46fc17
Burhan Nasir <burhandodhy@gmail.com>
2022-08-15 18:43:34 +0500

    Fix tests

--- tests/cypress/integration/features/terms.spec.js
+++ tests/cypress/integration/features/terms.spec.js
@@ -26,7 +26,10 @@ describe('Terms Feature', () => {
 			return true;
 		});
 
-		cy.get('.ep-sync-progress strong', { timeout: Cypress.config('elasticPressIndexTimeout') })
+		cy.get('.ep-sync-panel').last().as('syncPanel');
+		cy.get('@syncPanel').find('.components-form-toggle').click();
+		cy.get('@syncPanel')
+			.find('.ep-sync-messages', { timeout: Cypress.config('elasticPressIndexTimeout') })
 			.should('contain.text', 'Mapping sent')
 			.should('contain.text', 'Sync complete');
 
