e9015d1446a29a8faebdf230c269b0736967a0df
Jacob Peattie <jacob.peattie@10up.com>
2022-09-17 02:14:00 +1000

    Fix eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ const defaultEslintrc = require('10up-toolkit/config/.eslintrc');
 
 module.exports = {
 	...defaultEslintrc,
-	'jsdoc/check-tag-names': [
-		'error',
-		{
-			definedTags: ['filter', 'action'],
-		},
-	],
+	rules: {
+		...defaultEslintrc.rules,
+		'jsdoc/check-tag-names': [
+			'error',
+			{
+				definedTags: ['filter', 'action'],
+			},
+		],
+	},
 };
