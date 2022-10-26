b8a70c20e05c7d996883c00af560b6046fef7aea
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-10-26 10:09:24 -0600

    use Utils\*_option()

--- includes/classes/Features.php
+++ includes/classes/Features.php
@@ -133,11 +133,7 @@ class Features {
 
 		$sanitize_feature_settings = apply_filters( 'ep_sanitize_feature_settings', $feature_settings, $feature );
 
-		if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
-			update_site_option( 'ep_feature_settings', $sanitize_feature_settings );
-		} else {
-			update_option( 'ep_feature_settings', $sanitize_feature_settings );
-		}
+		Utils\update_option( 'ep_feature_settings', $sanitize_feature_settings );
 
 		$data = array(
 			'active'  => $sanitize_feature_settings[ $slug ]['active'],
@@ -194,11 +190,7 @@ class Features {
 		$is_wp_cli = defined( 'WP_CLI' ) && \WP_CLI;
 
 		if ( $is_wp_cli || is_admin() ) {
-			if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
-				update_site_option( 'ep_feature_requirement_statuses', $new_requirement_statuses );
-			} else {
-				update_option( 'ep_feature_requirement_statuses', $new_requirement_statuses );
-			}
+			Utils\update_option( 'ep_feature_requirement_statuses', $new_requirement_statuses );
 		}
 
 		/**
@@ -242,11 +234,7 @@ class Features {
 						$this->activate_feature( $slug );
 
 						if ( $feature->requires_install_reindex ) {
-							if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
-								update_site_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
-							} else {
-								update_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
-							}
+							Utils\update_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
 						}
 					}
 				} else {
@@ -259,11 +247,7 @@ class Features {
 
 							// Need to activate and maybe set a sync notice
 							if ( $feature->requires_install_reindex ) {
-								if ( defined( 'EP_IS_NETWORK' ) && EP_IS_NETWORK ) {
-									update_site_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
-								} else {
-									update_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
-								}
+								Utils\update_option( 'ep_feature_auto_activated_sync', sanitize_text_field( $slug ) );
 							}
 						} elseif ( $feature->is_active() && ! $active ) {
 							// Just deactivate, don't force
