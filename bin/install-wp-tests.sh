4f6fa25ec4737393278b4aa4d6f39d6d56164022
Felipe Elia <felipe.elia@gmail.com>
2022-10-27 12:30:56 -0300

    Change back one reference

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -42,7 +42,7 @@ elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
   WP_TESTS_TAG="trunk"
 else
   # http serves a single offer, whereas https serves multiple. we only want one
-  download https://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+  download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
   grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
   LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
   if [[ -z "$LATEST_VERSION" ]]; then
