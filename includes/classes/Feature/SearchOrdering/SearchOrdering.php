b44d0153202d046abe9c19cccbf7888397fbe081
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-11-08 17:14:45 -0700

    Pull in https://github.com/10up/ElasticPress/pull/3004 (#181)

--- includes/classes/Feature/SearchOrdering/SearchOrdering.php
+++ includes/classes/Feature/SearchOrdering/SearchOrdering.php
@@ -36,6 +36,11 @@ class SearchOrdering extends Feature {
 	const TAXONOMY_NAME = 'ep_custom_result';
 
 	/**
+	 * Capability required to manage.
+	 */
+	const CAPABILITY = 'manage_options';
+
+	/**
 	 * Initialize feature setting it's config
 	 *
 	 * @since  3.0
@@ -210,7 +215,7 @@ class SearchOrdering extends Feature {
 			'elasticpress',
 			esc_html__( 'Custom Results', 'elasticpress' ),
 			esc_html__( 'Custom Results', 'elasticpress' ),
-			'manage_options',
+			self::CAPABILITY,
 			'edit.php?post_type=' . self::POST_TYPE_NAME
 		);
 	}
@@ -685,7 +690,9 @@ class SearchOrdering extends Feature {
 			[
 				'methods'             => 'GET',
 				'callback'            => [ $this, 'handle_pointer_search' ],
-				'permission_callback' => '__return_true',
+				'permission_callback' => function() {
+					return current_user_can( self::CAPABILITY );
+				},
 				'args'                => [
 					's' => [
 						'validate_callback' => function ( $param ) {
@@ -703,7 +710,9 @@ class SearchOrdering extends Feature {
 			[
 				'methods'             => 'GET',
 				'callback'            => [ $this, 'handle_pointer_preview' ],
-				'permission_callback' => '__return_true',
+				'permission_callback' => function() {
+					return current_user_can( self::CAPABILITY );
+				},
 				'args'                => [
 					's' => [
 						'validate_callback' => function ( $param ) {
