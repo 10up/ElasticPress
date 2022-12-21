b8bd85b28f9e883baaff8b3b4cc5d3cfbf478826
Burhan Nasir <burhandodhy@gmail.com>
2022-12-21 12:08:47 +0500

    Adjust feedback

--- includes/classes/Feature.php
+++ includes/classes/Feature.php
@@ -352,6 +352,6 @@ abstract class Feature {
 	 * @return string
 	 */
 	public function get_epio_logo() : string {
-		return sprintf( '<img class="feature-epio-logo" alt="elasticpress.io-logo" src="%s">', esc_url( plugins_url( '/images/logo-elasticpress-io.svg', EP_FILE ) ) );
+		return sprintf( '<img class="feature-epio-logo" alt="ElasticPresss.io logo" src="%s" width="110" height="20">', esc_url( plugins_url( '/images/logo-elasticpress-io.svg', EP_FILE ) ) );
 	}
 }
