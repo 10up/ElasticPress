052ea5325baf4b3d11d6a56780e244cca19ac0ae
Jacob Peattie <jacob.peattie@10up.com>
2023-01-19 22:22:45 +1100

    Blocks/modal toggle.

--- assets/js/instant-results/index.js
+++ assets/js/instant-results/index.js
@@ -17,45 +17,49 @@ import Results from './components/layout/results';
  * Initialize Instant Results.
  */
 const init = () => {
+	const useBlocks = true;
 	const el = document.getElementById('ep-instant-results');
 
-	render(
-		<ApiSearchProvider
-			apiEndpoint={apiEndpoint}
-			apiHost={apiHost}
-			argsSchema={argsSchema}
-			paramPrefix={paramPrefix}
-		>
-			<Modal />
-		</ApiSearchProvider>,
-		el,
-	);
+	if (useBlocks) {
+		const facetEl = document.getElementById('ep-facet-block');
+		const resultsEl = document.getElementById('ep-results-block');
+		const searchEl = document.getElementById('ep-search-block');
 
-	const facetEl = document.getElementById('ep-facet-block');
-	const resultsEl = document.getElementById('ep-results-block');
-	const searchEl = document.getElementById('ep-search-block');
-
-	render(
-		<ApiSearchProvider
-			apiEndpoint={apiEndpoint}
-			apiHost={apiHost}
-			argsSchema={argsSchema}
-			paramPrefix={paramPrefix}
-		>
-			{createPortal(<SearchTermFacet />, searchEl)}
-			{createPortal(
-				<TaxonomyTermsFacet
-					defaultIsOpen
-					label="Category"
-					name="tax-category"
-					postTypes={[]}
-				/>,
-				facetEl,
-			)}
-			{createPortal(<Results />, resultsEl)}
-		</ApiSearchProvider>,
-		el,
-	);
+		render(
+			<ApiSearchProvider
+				apiEndpoint={apiEndpoint}
+				apiHost={apiHost}
+				argsSchema={argsSchema}
+				defaultIsOn
+				paramPrefix={paramPrefix}
+			>
+				{createPortal(<SearchTermFacet />, searchEl)}
+				{createPortal(
+					<TaxonomyTermsFacet
+						defaultIsOpen
+						label="Category"
+						name="tax-category"
+						postTypes={[]}
+					/>,
+					facetEl,
+				)}
+				{createPortal(<Results />, resultsEl)}
+			</ApiSearchProvider>,
+			el,
+		);
+	} else {
+		render(
+			<ApiSearchProvider
+				apiEndpoint={apiEndpoint}
+				apiHost={apiHost}
+				argsSchema={argsSchema}
+				paramPrefix={paramPrefix}
+			>
+				<Modal />
+			</ApiSearchProvider>,
+			el,
+		);
+	}
 };
 
 window.addEventListener('DOMContentLoaded', init);
