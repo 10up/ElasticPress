e3dc48dd05adb3eec6226646500f87c1e00ba9ac
Burhan Nasir <burhandodhy@gmail.com>
2023-01-25 17:47:03 +0500

    Prevent using the EP if query has unsuported orderby

--- tests/php/indexables/TestPost.php
+++ tests/php/indexables/TestPost.php
@@ -8337,4 +8337,39 @@ class TestPost extends BaseTestCase {
 		$ep_post = ElasticPress\Indexables::factory()->get( 'post' )->get( $product_id );
 		$this->assertArrayHasKey( '_thumbnail_id', $ep_post['meta'] );
 	}
+
+	/**
+	 * Test that query with unsupported orderby does not use EP.
+	 *
+	 * @since 4.5.0
+	 */
+	public function testQueryWithUnSupportedOrderByDoesNotUseEP() {
+		// test for post__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull( $query->elasticsearch_success  );
+
+		// test for post_name__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post_name__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull( $query->elasticsearch_success  );
+
+		// test for post_parent__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post_parent__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull(  $query->elasticsearch_success  );
+
+		// test for parent
+		$query = new \WP_Query( array(
+			'orderby' => 'parent',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull(  $query->elasticsearch_success  );
+	}
 }
