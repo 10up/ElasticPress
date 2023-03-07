e94d19bbb1a481890451df25fd5ceebe6ee25974
Felipe Elia <felipe.elia@get10up.com>
2023-03-07 10:44:04 -0300

    Check if is EP.io and valid response

--- includes/classes/ElasticPressIo.php
+++ includes/classes/ElasticPressIo.php
@@ -19,6 +19,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class ElasticPressIo {
 	/**
+	 * Name of the transient that stores EP.io messages
+	 */
+	const MESSAGES_TRANSIENT_NAME = 'ep_elasticpress_io_messages';
+
+	/**
 	 * Return singleton instance of class
 	 *
 	 * @return object
@@ -39,18 +44,33 @@ class ElasticPressIo {
 	 * @return array ElasticPress.io messages.
 	 */
 	public function get_endpoint_messages() {
-		$transient = 'ep_elasticpress_io_messages';
-		$messages  = get_transient( $transient );
+		if ( ! Utils\is_epio() ) {
+			return [];
+		}
 
+		$messages = get_transient( self::MESSAGES_TRANSIENT_NAME );
 		if ( false !== $messages ) {
 			return $messages;
 		}
 
 		$response = \ElasticPress\Elasticsearch::factory()->remote_request( 'endpoint-messages' );
+
+		$response_code = wp_remote_retrieve_response_code( $response );
+		if ( is_wp_error( $response ) || 200 !== $response_code ) {
+			return [];
+		}
+
 		$messages = (array) json_decode( wp_remote_retrieve_body( $response ), true );
 
-		set_transient( $transient, $messages, HOUR_IN_SECONDS );
+		set_transient( self::MESSAGES_TRANSIENT_NAME, $messages, HOUR_IN_SECONDS );
 
 		return $messages;
+	}
+
+	/**
+	 * Delete cached messages.
+	 */
+	public function delete_endpoint_messages() {
+		delete_transient( self::MESSAGES_TRANSIENT_NAME );
 	}
 }
