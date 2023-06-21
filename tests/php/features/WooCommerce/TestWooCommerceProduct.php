56f9602b0a6b31403c6438db201108962dfa29c4
Felipe Elia <felipe.elia@get10up.com>
2023-06-21 16:34:14 -0300

    More fixes

--- tests/php/features/WooCommerce/TestWooCommerceProduct.php
+++ tests/php/features/WooCommerce/TestWooCommerceProduct.php
@@ -50,4 +50,76 @@ class TestWooCommerceProduct extends BaseTestCase {
 		$this->assertContains( 'child-sku-1', $product_meta_to_index['_variations_skus'] );
 		$this->assertContains( 'child-sku-2', $product_meta_to_index['_variations_skus'] );
 	}
+
+	/**
+	 * Test the translate_args_admin_products_list method
+	 *
+	 * @group woocommerce
+	 */
+	public function testTranslateArgsAdminProductsList() {
+		ElasticPress\Features::factory()->activate_feature( 'protected_content' );
+		ElasticPress\Features::factory()->activate_feature( 'woocommerce' );
+		ElasticPress\Features::factory()->setup_features();
+
+		parse_str( 'post_type=product&s=product&product_type=downloadable&stock_status=instock', $_GET );
+
+		$query_args = [
+			'ep_integrate' => true,
+		];
+
+		$woocommerce_feature = ElasticPress\Features::factory()->get_registered_feature( 'woocommerce' );
+		add_action( 'pre_get_posts', [ $woocommerce_feature->products, 'translate_args_admin_products_list' ] );
+
+		$query = new \WP_Query( $query_args );
+
+		$this->assertTrue( $query->elasticsearch_success );
+		$this->assertEquals( $query->query_vars['s'], 'product' );
+		$this->assertEquals( $query->query_vars['meta_query'][0]['key'], '_downloadable' );
+		$this->assertEquals( $query->query_vars['meta_query'][0]['value'], 'yes' );
+		$this->assertEquals( $query->query_vars['meta_query'][1]['key'], '_stock_status' );
+		$this->assertEquals( $query->query_vars['meta_query'][1]['value'], 'instock' );
+		$this->assertEquals(
+			$query->query_vars['search_fields'],
+			[
+				'post_title',
+				'post_content',
+				'post_excerpt',
+				'meta' => [
+					'_sku',
+					'_variations_skus',
+				],
+			]
+		);
+	}
+
+	/**
+	 * Test the ep_woocommerce_admin_products_list_search_fields filter
+	 *
+	 * @group woocommerce
+	 */
+	public function testEPWoocommerceAdminProductsListSearchFields() {
+		ElasticPress\Features::factory()->activate_feature( 'protected_content' );
+		ElasticPress\Features::factory()->activate_feature( 'woocommerce' );
+		ElasticPress\Features::factory()->setup_features();
+
+		parse_str( 'post_type=product&s=product&product_type=downloadable', $_GET );
+
+		$query_args = [
+			'ep_integrate' => true,
+		];
+
+		$woocommerce_feature = ElasticPress\Features::factory()->get_registered_feature( 'woocommerce' );
+		add_action( 'pre_get_posts', [ $woocommerce_feature->products, 'translate_args_admin_products_list' ] );
+
+		$search_fields_function = function () {
+			return [ 'post_title', 'post_content' ];
+		};
+		add_filter( 'ep_woocommerce_admin_products_list_search_fields', $search_fields_function );
+
+		$query = new \WP_Query( $query_args );
+		$this->assertEquals(
+			$query->query_vars['search_fields'],
+			[ 'post_title', 'post_content' ]
+		);
+	}
 }
