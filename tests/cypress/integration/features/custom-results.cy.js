c5a14889d8a5d0b17d4be52ff0b866d3258e0443
Felipe Elia <felipe.elia@get10up.com>
2023-10-19 13:51:29 -0300

    Fix Custom Results

--- tests/cypress/integration/features/custom-results.cy.js
+++ tests/cypress/integration/features/custom-results.cy.js
@@ -41,27 +41,29 @@ describe('Custom Results', () => {
 		cy.wait('@ajaxRequest').its('response.statusCode').should('eq', 200);
 
 		// change the position of the post
-		// eslint-disable-next-line jest/valid-expect-in-promise
 		cy.dragAndDrop(
 			'.pointers .pointer:first-of-type .dashicons-menu',
 			'.pointers .pointer:last-of-type .dashicons-menu',
 		).then(() => {
 			// save the posts positions in a list
-			cy.get('.pointers .pointer .title').each((post) => {
-				searchResult.push(post[0].innerText);
-			});
+			// eslint-disable-next-line cypress/unsafe-to-chain-command
+			cy.get('.pointers .pointer .title')
+				.each((post) => {
+					searchResult.push(post[0].innerText);
+				})
+				.then(() => {
+					expect(searchResult.length).to.be.gt(0);
+					cy.get('#publish').click();
 
-			expect(searchResult.length).to.be.gt(0);
-			cy.get('#publish').click();
+					cy.visit(`?s=${searchTerm}`);
 
-			cy.visit(`?s=${searchTerm}`);
-
-			// verify the result of the search is in the same position.
-			cy.get(`article:nth-child(-n+${searchResult.length}) .entry-title`).each(
-				(post, index) => {
-					expect(post[0].innerText).to.equal(searchResult[index]);
-				},
-			);
+					// verify the result of the search is in the same position.
+					cy.get(`article:nth-child(-n+${searchResult.length}) .entry-title`).each(
+						(post, index) => {
+							expect(post[0].innerText).to.equal(searchResult[index]);
+						},
+					);
+				});
 		});
 	});
 
@@ -89,23 +91,27 @@ describe('Custom Results', () => {
 		cy.get('.pointer-result:first-of-type .dashicons-plus.add-pointer').click();
 
 		// save the posts positions in a list
-		// eslint-disable-next-line jest/valid-expect-in-promise
+		// eslint-disable-next-line cypress/unsafe-to-chain-command
 		cy.get('.pointers .pointer:nth-child(-n+5) .title') // 5 being the number of posts per page, as we will check only the first page.
 			.each((post) => {
 				searchResult.push(post[0].innerText);
-			});
-		expect(searchResult.length).to.be.gt(0);
-		cy.get('#publish').click();
-		/**
-		 * Give Elasticsearch some time to update the posts in custom results.
-		 */
-		// eslint-disable-next-line cypress/no-unnecessary-waiting
-		cy.wait(1000);
-		cy.visit(`?s=${searchTerm}`);
+			})
+			.then(() => {
+				expect(searchResult.length).to.be.gt(0);
+				cy.get('#publish').click();
+				/**
+				 * Give Elasticsearch some time to update the posts in custom results.
+				 */
+				// eslint-disable-next-line cypress/no-unnecessary-waiting
+				cy.wait(1000);
+				cy.visit(`?s=${searchTerm}`);
 
-		// verify the result of the search is in the same position.
-		cy.get(`article:nth-child(-n+${searchResult.length}) .entry-title`).each((post, index) => {
-			expect(post[0].innerText).to.equal(searchResult[index]);
-		});
+				// verify the result of the search is in the same position.
+				cy.get(`article:nth-child(-n+${searchResult.length}) .entry-title`).each(
+					(post, index) => {
+						expect(post[0].innerText).to.equal(searchResult[index]);
+					},
+				);
+			});
 	});
 });
