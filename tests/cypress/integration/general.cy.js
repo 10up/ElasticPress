e5709443c382335f98e726292c731772cf94db95
Felipe Elia <felipe.elia@get10up.com>
2025-02-27 17:28:41 -0300

    Try another way to exit the full screen mode

--- tests/cypress/integration/general.cy.js
+++ tests/cypress/integration/general.cy.js
@@ -41,7 +41,7 @@ describe('WordPress can perform standard ElasticPress actions', { tags: '@slow' 
 		});
 
 		// Exit full screen mode, so the admin bar is displayed.
-		cy.get('body').type('{ctrl+shift+alt}f');
+		cy.get('body').type('{ctrl+shift+alt+f');
 		cy.get('#wp-admin-bar-ep-doc-status').should('contain.text', 'Content in sync');
 
 		cy.visit('/?s=Test+ElasticPress+1');
