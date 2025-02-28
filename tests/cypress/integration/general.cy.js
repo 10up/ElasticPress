51b7bb2e7a5b0f14d6590f25775d99eea2a1d618
Felipe Elia <felipe.elia@get10up.com>
2025-02-28 07:31:12 -0300

    Reload the page

--- tests/cypress/integration/general.cy.js
+++ tests/cypress/integration/general.cy.js
@@ -40,8 +40,7 @@ describe('WordPress can perform standard ElasticPress actions', { tags: '@slow' 
 			title: 'Test ElasticPress 1',
 		});
 
-		// Exit full screen mode, so the admin bar is displayed.
-		cy.get('body').type('{ctrl+shift+alt+f}');
+		cy.reload();
 		cy.get('#wp-admin-bar-ep-doc-status').should('contain.text', 'Content in sync');
 
 		cy.visit('/?s=Test+ElasticPress+1');
