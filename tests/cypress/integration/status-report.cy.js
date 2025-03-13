ab4ab5ed09ca93087ae8d26cfff691b33988376e
Oscar Sanchez S. <oscar@oscarssanchez.com>
2025-03-13 21:44:55 +0700

    Consolidate in one test

--- tests/cypress/integration/status-report.cy.js
+++ tests/cypress/integration/status-report.cy.js
@@ -3,32 +3,22 @@ describe('Status Report', () => {
 		cy.visitAdminPage('admin.php?page=elasticpress-status-report');
 	});
 
-	it('should have a notice component for AJAX reports instead of the content', () => {
+	it('should have a notice component for AJAX reports instead of the content, buttons should not be disabled by default, should load report after click on full report, and generate report should be disabled after clicking it', () => {
 		cy.get('#indexable').parent().parent().find('.components-notice__content').should('exist');
-	});
 
-	it('should have all three buttons enabled to click on first load', () => {
 		cy.get('#download-report').should('not.be.disabled');
 		cy.get('#copy-report').should('not.be.disabled');
 		cy.get('#generate-full-report').should('not.be.disabled');
-	});
 
-	it('should disable the generate report button after clicking it', () => {
 		cy.get('#generate-full-report').click();
 		cy.get('#generate-full-report').should('be.disabled');
-	});
 
-	it('should replace the notice component with the content after clicking the generate report button', () => {
-		cy.get('#generate-full-report').click();
 		cy.get('#indexable')
 			.parent()
 			.parent()
 			.find('.components-notice__content')
 			.should('not.exist');
-	});
 
-	it('should have changed the copy and download button text after clicking on generate full report', () => {
-		cy.get('#generate-full-report').click();
 		cy.get('#download-report').should('have.text', 'Download full status report');
 		cy.get('#copy-report').should('have.text', 'Copy full status report to clipboard');
 	});
