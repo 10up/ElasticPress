82170db29836a29c784246558bcbf50d898ec918
Zachary Rener <me@zacharyrener.com>
2025-04-23 13:48:39 -0500

    Add unit & e2e tests for feature grouping

---
+++ tests/cypress/integration/features/groups.cy.js
@@ -0,0 +1,74 @@
+describe('Feature Grouping', () => {
+	/**
+	 * CSS selector for the tabs container within the ElasticPress dashboard.
+	 * @constant {string}
+	 */
+	const tabContainerSelector =
+		'#ep-dashboard .ep-dashboard-outer-tabs > .components-tab-panel__tabs';
+
+	beforeEach(() => {
+		/**
+		 * Log in to WordPress admin.
+		 */
+		cy.login();
+
+		/**
+		 * Navigate to the ElasticPress settings page.
+		 */
+		cy.visit('/wp-admin/admin.php?page=elasticpress');
+
+		/**
+		 * Alias the tabs container for reuse in tests.
+		 */
+		cy.get(tabContainerSelector).as('tabsContainer');
+	});
+
+	it('renders feature group tabs', () => {
+		/**
+		 * Verifies the tabs container exists, is visible,
+		 * and contains at least one feature tab.
+		 */
+		cy.get('@tabsContainer')
+			.should('exist')
+			.and('be.visible')
+			.find('button')
+			.should('have.length.at.least', 1);
+	});
+
+	it('switches content when clicking a tab', () => {
+		/**
+		 * Clicks the second tab and verifies the corresponding
+		 * panel is opened by checking the data-open attribute.
+		 */
+		// eslint-disable-next-line cypress/unsafe-to-chain-command
+		cy.get('@tabsContainer')
+			.find('button')
+			.eq(1)
+			.click()
+			.then(($btn) => {
+				/** @type {string} ID of the clicked tab button. */
+				const buttonId = $btn.attr('id');
+				// eslint-disable-next-line no-unused-expressions
+				expect(buttonId, 'tab button id').to.be.a('string').and.not.be.empty;
+
+				/**
+				 * Create the panel ID and escape any special characters (including spaces).
+				 * Uses the built-in CSS.escape() to safely escape the ID value.
+				 * @constant {string}
+				 */
+				const rawPanelId = `${buttonId}-view`;
+				const escapedPanelId = CSS.escape(rawPanelId);
+
+				/**
+				 * Construct the panel selector using the escaped ID.
+				 * @constant {string}
+				 */
+				const panelSelector = `#${escapedPanelId}`;
+
+				/**
+				 * Assert that the panel element has data-open="true".
+				 */
+				cy.get(panelSelector).should('have.attr', 'data-open', 'true');
+			});
+	});
+});
