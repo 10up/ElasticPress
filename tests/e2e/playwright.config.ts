558ec921b6ea3c63ba6c0ae2553ab365a4229e35
Felipe Elia <felipe.elia@get10up.com>
2025-04-29 12:38:40 -0300

    (somewhat) dynamic baseURL

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:8889',
+		baseURL: process.env.CI ? 'http://172.17.0.1:8890' : 'http://localhost:8889',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
