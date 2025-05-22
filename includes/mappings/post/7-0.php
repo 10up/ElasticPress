dae98513f2a8a487e7f1bdaec0520635dc17c80b
Rebecca Hum <rebeccahum@gmail.com>
2025-05-22 10:47:00 -0600

    ES 8.16 compatibility: Remove side from edge_ngram tokenizer

--- includes/mappings/post/7-0.php
+++ includes/mappings/post/7-0.php
@@ -138,7 +138,6 @@ return array(
 					'language' => apply_filters( 'ep_analyzer_language', 'english', 'filter_ewp_snowball' ),
 				),
 				'edge_ngram'     => array(
-					'side'     => 'front',
 					'max_gram' => 10,
 					'min_gram' => 3,
 					'type'     => 'edge_ngram',
