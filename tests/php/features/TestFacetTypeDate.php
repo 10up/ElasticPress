d86daa485c346d598f9ca9d7a97931d597096e68
Burhan Nasir <burhan.nasir@10up.com>
2025-05-23 17:50:15 +0500

    Update tests

--- tests/php/features/TestFacetTypeDate.php
+++ tests/php/features/TestFacetTypeDate.php
@@ -369,4 +369,22 @@ class TestFacetTypeDate extends BaseTestCase {
 		$this->assertTrue( $query->elasticsearch_success );
 		$this->assertEquals( 5, $query->found_posts );
 	}
+
+	/**
+	 * Test the block does not register in the editor
+	 *
+	 * @since 5.3.0
+	 * @group facets
+	 */
+	public function test_block_does_not_register_in_editor() {
+		$GLOBALS['pagenow'] = 'post-new.php';
+		set_current_screen( 'post-new.php' );
+
+		$facet_feature = Features::factory()->get_registered_feature( 'facets' );
+		$facet_feature->tear_down();
+		$facet_feature->setup();
+		$facet_type = $facet_feature->types['date'];
+
+		$this->assertFalse( has_action( 'init', [ $facet_type->block, 'register_block' ] ) );
+	}
 }
