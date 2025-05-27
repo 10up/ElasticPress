4fd1d2dbc630467a780fd9e21690fe287f0a2f25
Zachary Rener <me@zacharyrener.com>
2025-05-27 15:44:48 -0500

    Update test

--- tests/cypress/integration/features/interface.cy.js
+++ tests/cypress/integration/features/interface.cy.js
@@ -52,56 +52,56 @@ describe('Feature Grouping and Persistence', () => {
 						cy.get('div[id*="autosuggest-view"]').should('be.visible');
 					});
 			});
+
+		cy.visit('/wp-admin/admin.php?page=elasticpress');
+
+		cy.window()
+			.then((win) => {
+				// Wait until epDashboard and features are available
+				return new Cypress.Promise((resolve) => {
+					const check = () => {
+						if (win.epDashboard && win.epDashboard.features) {
+							resolve(win);
+						} else {
+							setTimeout(check, 50);
+						}
+					};
+					check();
+				});
+			})
+			.then((win) => {
+				win.epDashboard.features[2].settingsSchema.push({
+					type: 'field_group',
+					key: 'fieldgroupa',
+					label: 'Field Group ABC',
+					fields: [
+						{
+							default: '.ep-autosuggest',
+							help: 'Input additional selectors where you would like to include autosuggest, separated by a comma. Example: <code>.custom-selector, #custom-id, input[type="text"]</code>',
+							key: 'autosuggest_selector',
+							label: 'Additional selectors',
+							type: 'text',
+						},
+						{
+							default: '0',
+							key: 'trigger_ga_event',
+							help: 'Enable to fire a gtag tracking event when an autosuggest result is clicked.',
+							label: 'Trigger Google Analytics events',
+							type: 'checkbox',
+						},
+					],
+				});
+			});
+
+		/**
+		 * Navigate to the correct feature group and subfeature
+		 */
+		cy.contains('button', 'WooCommerce').click();
+		cy.contains('button', 'Live Search').click();
+		cy.contains('button', 'Autosuggest').click();
+
+		cy.contains('.ep-field-group', 'Field Group ABC').as('fieldGroup');
+		cy.get('@fieldGroup').should('exist');
+		cy.get('@fieldGroup').find('.ep-dashboard-control').should('exist');
 	});
-
-	cy.visit('/wp-admin/admin.php?page=elasticpress');
-
-	cy.window()
-		.then((win) => {
-			// Wait until epDashboard and features are available
-			return new Cypress.Promise((resolve) => {
-				const check = () => {
-					if (win.epDashboard && win.epDashboard.features) {
-						resolve(win);
-					} else {
-						setTimeout(check, 50);
-					}
-				};
-				check();
-			});
-		})
-		.then((win) => {
-			win.epDashboard.features[2].settingsSchema.push({
-				type: 'field_group',
-				key: 'fieldgroupa',
-				label: 'Field Group ABC',
-				fields: [
-					{
-						default: '.ep-autosuggest',
-						help: 'Input additional selectors where you would like to include autosuggest, separated by a comma. Example: <code>.custom-selector, #custom-id, input[type="text"]</code>',
-						key: 'autosuggest_selector',
-						label: 'Additional selectors',
-						type: 'text',
-					},
-					{
-						default: '0',
-						key: 'trigger_ga_event',
-						help: 'Enable to fire a gtag tracking event when an autosuggest result is clicked.',
-						label: 'Trigger Google Analytics events',
-						type: 'checkbox',
-					},
-				],
-			});
-		});
-
-	/**
-	 * Navigate to the correct feature group and subfeature
-	 */
-	cy.contains('button', 'WooCommerce').click();
-	cy.contains('button', 'Live Search').click();
-	cy.contains('button', 'Autosuggest').click();
-
-	cy.contains('.ep-field-group', 'Field Group ABC').as('fieldGroup');
-	cy.get('@fieldGroup').should('exist');
-	cy.get('@fieldGroup').find('.ep-dashboard-control').should('exist');
 });
