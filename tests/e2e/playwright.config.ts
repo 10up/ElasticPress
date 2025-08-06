adb96f8948b395531d7b54cdf32fa08d6db83456
Felipe Elia <felipe.elia@get10up.com>
2025-08-06 15:49:33 -0300

    EP.io + Screenshots and videos

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -30,6 +30,12 @@ export default defineConfig({
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
+
+		// Capture screenshot after each test failure.
+		screenshot: 'only-on-failure',
+
+		// Record video only when retrying a test for the first time.
+		video: 'retain-on-failure',
 	},
 
 	/* Configure projects for major browsers */
