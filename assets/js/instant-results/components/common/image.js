f754ddbeaf15cd6a51cd9d44114b53705d371109
Burhan Nasir <burhan.nasir@10up.com>
2025-08-20 14:47:51 +0500

    Adjust feedback

--- assets/js/instant-results/components/common/image.js
+++ assets/js/instant-results/components/common/image.js
@@ -4,12 +4,42 @@
 import { WPElement } from '@wordpress/element';
 
 /**
+ * Calculate the sizes attribute.
+ *
+ * @param {number} width Image width
+ * @param {number} height Image height
+ * @param {number} containerWidth Container width
+ * @returns {string} Sizes attribute value
+ */
+const calculateSizes = (width, height, containerWidth) => {
+	if (!width || !height || width <= 0 || height <= 0) {
+		return `${containerWidth}px`;
+	}
+
+	const effectiveWidth = Math.max((width / height) * containerWidth, containerWidth);
+
+	return `${Math.round(effectiveWidth)}px`;
+};
+
+/**
  * Image component.
  *
  * @param {Option} props Component props.
  *
  * @returns {WPElement} Component element.
  */
-export default ({ alt, height, ID, src, width, srcset, ...props }) => {
-	return <img alt={alt} src={src} width={width} height={height} srcSet={srcset} {...props} />;
+export default ({ alt, height, ID, src, width, srcset, containerWidth = 300, ...props }) => {
+	const sizes = calculateSizes(width, height, containerWidth);
+
+	return (
+		<img
+			alt={alt}
+			src={src}
+			width={width}
+			height={height}
+			srcSet={srcset}
+			sizes={sizes}
+			{...props}
+		/>
+	);
 };
