c2c2350a248296adef3b974ddd17a2d74e53aea8
Burhan Nasir <burhan.nasir@10up.com>
2026-08-07 10:39:23 +0500

    Set version to 6.2

--- tests/e2e/src/utils.ts
+++ tests/e2e/src/utils.ts
@@ -265,13 +265,13 @@ export async function getEditorFrame(page: Page): Promise<Page | FrameLocator> {
 
 export async function maybeOpenEditorSettings(page: Page) {
 	const selector =
-		process.env.WP_VERSION === '6.4'
+		process.env.WP_VERSION === '6.2'
 			? '.interface-interface-skeleton__sidebar .interface-complementary-area'
 			: '.interface-interface-skeleton__sidebar .interface-complementary-area__fill';
 	const editorSettings = page.locator(selector);
 
 	try {
-		if (process.env.WP_VERSION !== '6.4') {
+		if (process.env.WP_VERSION !== '6.2') {
 			await editorSettings.waitFor({ state: 'visible', timeout: 5 });
 		}
 	} catch (error) {
@@ -292,7 +292,7 @@ export async function maybeOpenSettingsTab(page: Page, tabName: string) {
 
 	let tab: Locator;
 	let isTabActive: string | boolean | null;
-	if (process.env.WP_VERSION === '6.4') {
+	if (process.env.WP_VERSION === '6.2') {
 		tab = page.locator('.edit-post-sidebar__panel-tab, .edit-widgets-sidebar__panel-tab', {
 			hasText: tabName,
 		});
@@ -323,7 +323,7 @@ export async function setPostPassword(
 	await maybeOpenEditorSettings(page);
 	await maybeOpenSettingsTab(page, 'Post');
 
-	if (process.env.WP_VERSION === '6.4') {
+	if (process.env.WP_VERSION === '6.2') {
 		await page.locator('.edit-post-post-visibility__toggle').click();
 		await page
 			.getByRole('radio', { name: password !== '' ? 'Password protected' : 'Public' })
@@ -406,7 +406,7 @@ export async function publishPost(
 		if (!isInCodeEditorMode) {
 			await changeMode(page);
 		}
-		if (process.env.WP_VERSION === '6.4') {
+		if (process.env.WP_VERSION === '6.2') {
 			await page.locator('.wp-block-post-title').fill(newPostData.title);
 		} else {
 			await page.locator('.wp-block-post-title textarea').fill(newPostData.title);
